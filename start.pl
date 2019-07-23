655ab44be11d815a459dc7ec05d3f1cceb6e0079
Gavin Mendel-Gleason <gavin.mendel.gleason@gmail.com>
2019-07-23 17:27:10 +0100

    Adding first reference

--- start.pl
+++ start.pl
@@ -1,5 +1,23 @@
 #!/usr/bin/env swipl
 
+/* 
+    This file is part of RegulumDB.
+
+    RegulumDB is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RegulumDB is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with RegulumDB.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 :- initialization(main).
 
 :- dynamic user:file_search_path/2.
