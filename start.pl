cfb857df4896fad8fd3bc48e367aad65f860d997
Gavin Mendel-Gleason <gavin.mendel.gleason@gmail.com>
2019-10-01 17:07:22 +0100

    GPL version strip

--- start.pl
+++ start.pl
@@ -5,7 +5,7 @@
  *
  *  TerminusDB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
+ *  the Free Software Foundation, under version 3 of the License, or
  *  (at your option) any later version.
  *
  *  TerminusDB is distributed in the hope that it will be useful,
