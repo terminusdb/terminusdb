e19f8a1f63e56f150b21b0f77adf345b36a4586a
Gavin Mendel-Gleason <gavin.mendel.gleason@gmail.com>
2020-01-08 10:24:54 +0000

    Move www_form_encode to uri_encoded which is not deprecated

--- library/api.pl
+++ library/api.pl
@@ -733,7 +733,7 @@ try_db_uri(DB,DB_URI) :-
  * Die if we can't form a document uri.
  */
 try_doc_uri(DB_URI,Doc_ID,Doc_URI) :-
-    www_form_encode(Doc_ID,Doc_ID_Safe),
+    uri_encoded(fragment,Doc_ID,Doc_ID_Safe),
     (   interpolate([DB_URI,'/',document, '/',Doc_ID_Safe],Doc_URI)
     ->  true
     ;   format(atom(MSG), 'Document resource ~s can not be constructed in ~s', [DB_URI,Doc_ID]),
