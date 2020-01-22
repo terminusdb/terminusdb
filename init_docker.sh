7b25e2cca9bccf32ca0be1ad92df6e7b0d15174e
rrooij <robin@datachemist.com>
2020-01-22 14:00:19 +0100

    Rename ingress url to public url
    Since ingresses are not the only way of exposing
    the server to the public. E.g. a reverse proxy
    like nginx can also be used.

--- init_docker.sh
+++ init_docker.sh
@@ -3,11 +3,11 @@ SERVER_NAME=${SERVER_NAME:-localhost}
 ADMIN_PASS=${ADMIN_PASS:-root}
 SERVER_PORT=${SERVER_PORT:-6363}
 WORKERS=${WORKERS:-8}
-INGRESS_URL=${INGRESS_URL:-false}
+PUBLIC_URL=${PUBLIC_URL:-false}
 if [ ! -f /app/terminusdb/storage/prefix.db ]; then
-    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --ingress_url "$INGRESS_URL"
+    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL"
 fi
 if [ ! -f /app/terminusdb/config/config.pl ]; then
-    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --ingress_url "$INGRESS_URL" --only-config
+    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL" --only-config
 fi
 /app/terminusdb/start.pl
