3e36a20ee84c90e859c0fc7adbbf32cf3fbfbbda
Gavin Mendel-Gleason <gavin.mendel.gleason@gmail.com>
2020-01-22 10:57:15 +0000

    Add ingress URL option to docker

--- init_docker.sh
+++ init_docker.sh
@@ -3,8 +3,9 @@ SERVER_NAME=${SERVER_NAME:-localhost}
 ADMIN_PASS=${ADMIN_PASS:-root}
 SERVER_PORT=${SERVER_PORT:-6363}
 WORKERS=${WORKERS:-8}
+INGRESS_URL=${INGRESS_URL:-false}
 if [ ! -f /app/terminusdb/storage/prefix.db ]; then
-    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS"
+    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --ingress_url "$INGRESS_URL"
 fi
 if [ ! -f /app/terminusdb/config/config.pl ]; then
     /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --only-config
