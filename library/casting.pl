85c432a01a49f67ac1ccc2eee1835d74a78ec79b
Gavin Mendel-Gleason <gavin.mendel.gleason@gmail.com>
2020-02-07 20:46:33 +0000

    More choice point removal

--- library/casting.pl
+++ library/casting.pl
@@ -103,7 +103,7 @@ typecast_switch(Val, _ST, 'http://www.w3.org/2002/07/owl#Thing', _, Val) :-
     !.
 typecast_switch(Val, _ST, 'http://www.w3.org/2001/XMLSchema#dateTime', _, Cast) :-
     (   guess_date(Val,Cast)
-    ->  true
+    ->  !
     ;   format(atom(M),'Unable to cast as (xsd:dateTime): ~q~n',
                [Val]),
         term_jsonld(Val,JVal),
@@ -113,7 +113,7 @@ typecast_switch(Val, _ST, 'http://www.w3.org/2001/XMLSchema#dateTime', _, Cast) 
     ).
 typecast_switch(Val, _ST, 'http://www.w3.org/2001/XMLSchema#integer', _, Cast) :-
     (   guess_integer(Val,Cast)
-    ->  true
+    ->  !
     ;   format(atom(M),'Unable to cast as (xsd:integer): ~q~n',
                [Val]),
         term_jsonld(Val,JVal),
