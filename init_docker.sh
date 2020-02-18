6b17b8a104b0c7d0a4433a3535c5c9e8882dc153
Dmytri Kleiner <dk@trick.ca>
2020-02-18 15:29:23 +0100

    only use --no-tty in serve mode

--- init_docker.sh
+++ init_docker.sh
@@ -7,8 +7,14 @@ WORKERS=${WORKERS:-8}
 PUBLIC_URL=${PUBLIC_URL:-false}
 AUTOLOGIN=${AUTOLOGIN:-false} # Either true or false
 if [ ! -f /app/terminusdb/storage/prefix.db ]; then
-    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL" --autologin=$AUTOLOGIN
+    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL" --autologin="$AUTOLOGIN"
 else
-    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL" --autologin=$AUTOLOGIN --only-config
+    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL" --autologin="$AUTOLOGIN" --only-config
 fi
-nohup swipl --no-tty --quiet -f "$FILE_DIR"/start.pl $SERVER_MODE
+
+if [ "$SERVER_MODE" = "serve" ]; then 
+  nohup swipl --no-tty --quiet -f "$FILE_DIR"/start.pl "$SERVER_MODE"
+else
+  /app/terminusdb/start.pl "$SERVER_MODE"
+fi
+
