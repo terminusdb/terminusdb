4038f9558e1c16d25df325909d741472c7dbc911
rrooij <robin@datachemist.com>
2020-03-16 11:08:11 +0100

    Change reexport calls
    Seems to be recommended by the SWI docs
    
    https://www.swi-prolog.org/pldoc/man?section=reexport

--- core/query.pl
+++ core/query.pl
@@ -1,15 +1,14 @@
 :- module(query, []).
 
-:- reexport(query/expansions).
-:- reexport(query/frame).
-:- reexport(query/frame_types).
-:- reexport(query/inference).
-
+:- reexport([query/expansions,
+             query/frame,
+             query/frame_types,
+             query/inference,
 % dubious - why are we working with json so far from the api?
 % included in this module because various predicates in query itself depend on it.
 % Those bits that depend on it should probably be in api instead.
-:- reexport(query/jsonld).
-:- reexport(query/json_woql).
-
-:- reexport(query/ask).
-:- reexport(query/woql_compile).
+             query/jsonld,
+             query/json_woql,
+             query/ask,
+             query/woql_compile
+            ]).
--- core/util.pl
+++ core/util.pl
@@ -1,10 +1,11 @@
 :- module(util, []).
 
 % note: test_utils is intentionally omitted
-:- reexport(util/syntax).
-:- reexport(util/file_utils).
-:- reexport(util/types).
-:- reexport(util/remote_file).
-:- reexport(util/utils).
-:- reexport(util/speculative_parse).
-:- reexport(util/xsd_parser).
+:- reexport([util/syntax,
+             util/file_utils,
+             util/types,
+             util/remote_file,
+             util/utils,
+             util/speculative_parse,
+             util/xsd_parser
+            ]).
