797a910d55be6a960ce1908fe6fc735f5491608d
rrooij <robin@datachemist.com>
2020-03-30 14:25:16 +0200

    init_docker: prefix all env variables with terminus

--- init_docker.sh
+++ init_docker.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
-SERVER_MODE=${SERVER_MODE:-attach}
-SERVER_NAME=${SERVER_NAME:-localhost}
-ADMIN_PASS=${ADMIN_PASS:-root}
-SERVER_PORT=${SERVER_PORT:-6363}
-WORKERS=${WORKERS:-8}
-PUBLIC_URL=${PUBLIC_URL:-false}
-AUTOATTACH=${AUTOATTACH:-true}
-AUTOLOGIN=${AUTOLOGIN:-false}
+TERMINUS_SERVER_MODE=${TERMINUS_SERVER_MODE:-attach}
+TERMINUS_SERVER_NAME=${TERMINUS_SERVER_NAME:-localhost}
+TERMINUS_ADMIN_PASS=${TERMINUS_ADMIN_PASS:-root}
+TERMINUS_SERVER_PORT=${TERMINUS_SERVER_PORT:-6363}
+TERMINUS_PUBLIC_URL=${TERMINUS_PUBLIC_URL:-"http://localhost:6363"}
+TERMINUS_AUTOATTACH=${TERMINUS_AUTOATTACH:-true}
+TERMINUS_AUTOLOGIN=${TERMINUS_AUTOLOGIN:-false}
 TERMINUS_ENABLE_WELCOME_SCREEN=${TERMINUS_ENABLE_WELCOME_SCREEN:-false}
 
 if [ ! -f /app/terminusdb/storage/prefix.db ] && [ "$TERMINUS_ENABLE_WELCOME_SCREEN" = false ]; then
-    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL" --autologin="$AUTOLOGIN" --autoattach="$AUTOATTACH"
+    /app/terminusdb/utils/db_util -s "$TERMINUS_SERVER_NAME" -k "$TERMINUS_ADMIN_PASS" --port "$TERMINUS_SERVER_PORT" --public_url "$TERMINUS_PUBLIC_URL" --autologin="$TERMINUS_AUTOLOGIN" --autoattach="$TERMINUS_AUTOATTACH"
 elif [ "$TERMINUS_ENABLE_WELCOME_SCREEN" = false ]; then
-    /app/terminusdb/utils/db_util -s "$SERVER_NAME" -k "$ADMIN_PASS" --port "$SERVER_PORT" --workers "$WORKERS" --public_url "$PUBLIC_URL" --autologin="$AUTOLOGIN" --autoattach="$AUTOATTACH" --only-config
+    /app/terminusdb/utils/db_util -s "$TERMINUS_SERVER_NAME" -k "$TERMINUS_ADMIN_PASS" --port "$SERVER_PORT" --public_url "$TERMINUS_PUBLIC_URL" --autologin="$TERMINUS_AUTOLOGIN" --autoattach="$TERMINUS_AUTOATTACH" --only-config
 fi
 
 if [ "$TERMINUS_ENABLE_WELCOME_SCREEN" = true ]; then
-    cd /app/terminusdb/utils && swipl welcome_screen.pl $SERVER_PORT
+    cd /app/terminusdb/utils && swipl welcome_screen.pl $TERMINUS_SERVER_PORT
 fi
 
-echo "SERVER_MODE $SERVER_MODE"
-echo "SERVER_NAME $SERVER_NAME"
-echo "SERVER_PORT $SERVER_PORT"
-echo "PUBLIC_URL $PUBLIC_URL"
-echo "AUTO_ATTACH $AUTOATTACH"
-echo "AUTO_LOGIN $AUTOLOGIN"
+echo "SERVER_MODE $TERMINUS_SERVER_MODE"
+echo "SERVER_NAME $TERMINUS_SERVER_NAME"
+echo "SERVER_PORT $TERMINUS_SERVER_PORT"
+echo "PUBLIC_URL $TERMINUS_PUBLIC_URL"
+echo "AUTO_ATTACH $TERMINUS_AUTOATTACH"
+echo "AUTO_LOGIN $TERMINUS_AUTOLOGIN"
 echo "ENABLE_WELCOME_SCREEN $TERMINUS_ENABLE_WELCOME_SCREEN"
-/app/terminusdb/start.pl "$SERVER_MODE"
+/app/terminusdb/start.pl "$TERMINUS_SERVER_MODE"
 
