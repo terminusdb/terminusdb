2556ff168ee32b8969bed6a47ef3763b5e09b7e0
rrooij <robin@datachemist.com>
2020-04-06 09:53:59 +0200

    capabilities: make username_auth consistent with key_auth

--- core/account/capabilities.pl
+++ core/account/capabilities.pl
@@ -105,10 +105,7 @@ user_key_auth(DB, Username, Key, Auth_ID) :-
  */
 username_auth(DB, Username, Auth) :-
     username_user_id(DB, Username, User_ID),
-    user_auth_id(DB, User_ID, Auth_ID),
-    collection_descriptor_prefixes(DB.descriptor, Prefixes),
-    prefixed_to_uri(Auth_ID, Prefixes, Auth_ID_Expanded),
-    document_jsonld(DB,Auth_ID_Expanded,Auth).
+    user_auth_id(DB, User_ID, Auth).
 
 /*
  * user_auth_id(+DB, +User_ID, -Auth_id) is semidet.
