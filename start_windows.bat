7d3d7df08f23bbb338dfeb1b97b83b9184217851
rrooij <robin@datachemist.com>
2020-04-12 18:12:43 +0200

    windows: try to include swipl in installer already

--- start_windows.bat
+++ start_windows.bat
@@ -1,3 +1,4 @@
+set ScriptDir=%~dp0
 mkdir "%APPDATA%\TerminusDB"
 mkdir "%APPDATA%\TerminusDB\tmp"
 set TERMINUS_SERVER_DB_PATH=%APPDATA%\TerminusDB\db
@@ -5,5 +6,5 @@ set TERMINUS_SERVER_REGISTRY_PATH=%APPDATA%\TerminusDB\registry.pl
 set TERMINUS_SERVER_TMP_PATH=%APPDATA%\TerminusDB\tmp
 set TERMINUS_SERVER_INDEX_PATH=%APPDATA%\TerminusDB\index.html
 set TERMINUS_LOG_PATH=%APPDATA%\TerminusDB\httpd.log
-if not exist %TERMINUS_SERVER_DB_PATH% swipl utils\db_util -s localhost -k root --autologin=true
-swipl start.pl
+if not exist %TERMINUS_SERVER_DB_PATH% "%ScriptDir%swipl\bin\swipl" utils\db_util -s localhost -k root --autologin=true
+"%ScriptDir%swipl\bin\swipl.exe" start.pl
