5c3c51257f6181bb969e777e78709323519a6f06
Gavin Mendel-Gleason <jacobian@gmail.com>
2020-05-26 16:02:13 +0200

    Adding clone

---
+++ core/api/db_clone.pl
@@ -0,0 +1,48 @@
+:- module(db_clone, [
+              clone/7
+          ]).
+
+
+:- use_module(core(util)).
+:- use_module(core(query)).
+:- use_module(core(triple)).
+:- use_module(core(transaction)).
+:- use_module(db_init).
+:- use_module(db_fetch).
+:- use_module(db_fast_forward).
+
+:- meta_predicate clone(+,+,+,+,+,3,-).
+clone(Account,DB,Label,Comment,Remote_URL,Fetch_Predicate,Meta_Data) :-
+    % Create DB
+    user_database_name(Account, DB, DB_Name),
+    try_create_db(DB_Name, Label, Comment, _{}),
+    % Add remote
+    with_transaction(
+        Database_Context,
+        insert_remote_repository(Database_Context,
+                                 "origin",
+                                 Remote_URL,
+                                 _Head,
+                                 _Repo_URI
+                                ),
+        _Meta_Data),
+
+    resolve_absolute_descriptor([Account,DB,"local","_commits"], To_Descriptor),
+    resolve_absolute_descriptor([Account,DB,"origin","_commits"], From_Descriptor),
+
+    % Fetch remote
+    remote_fetch(From_Descriptor, Fetch_Predicate, _New_Head, _Has_Updated),
+
+    create_context(To_Descriptor, To_Context),
+    with_transaction(
+        To_Context,
+        % Copy Prefixes
+        copy_prefixes(From_Descriptor, To_Context),
+        _),
+
+    resolve_absolute_descriptor([Account,DB,"local","branch", "master"], To_Branch_Descriptor),
+    resolve_absolute_descriptor([Account,DB,"origin","branch", "master"], From_Branch_Descriptor),
+
+    % Fast forward commits from master in remote to master in local
+    fast_forward_branch(To_Branch_Descriptor, From_Branch_Descriptor, Applied_Commits),
+    Meta_Data = _{ applied_commits : Applied_Commits }.
