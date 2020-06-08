d632b956af9a201896def98f229c7baee12b6505
Gavin Mendel-Gleason <jacobian@gmail.com>
2020-06-08 13:51:08 +0200

    Fixing bugs in db_push for branch with no commit

--- core/api/db_push.pl
+++ core/api/db_push.pl
@@ -47,41 +47,47 @@ push(Branch_Descriptor, Remote_Name, Remote_Branch, _Auth_ID,
         context_default_prefixes(Remote_Repository_Context_With_Prefixes,
                                  Remote_Repository_Context),
         context_to_parent_transaction(Remote_Repository_Context, Database_Transaction),
-        * test_utils:print_all_triples(Database_Transaction),
         do_or_die(repository_head(Database_Transaction, Remote_Name, Last_Head_Id),
                   error(push_has_no_repository_head(Remote_Repository)))
-    ->  branch_head_commit(Repository_Context,
-                           (Branch_Descriptor.branch_name),
-                           Local_Commit_Uri),
-        (   has_branch(Remote_Repository_Context,
-                       Remote_Branch)
-        ->  (   branch_head_commit(Remote_Repository_Context,
-                                   Remote_Branch,
-                                   Remote_Commit_Uri)
-            ->  Remote_Commit_Uri_Option = some(Remote_Commit_Uri)
-            ;   Remote_Commit_Uri_Option = none
+    ->  (   branch_head_commit(Repository_Context,
+                               (Branch_Descriptor.branch_name),
+                               Local_Commit_Uri)
+        ->  (   has_branch(Remote_Repository_Context,
+                           Remote_Branch)
+            ->  (   branch_head_commit(Remote_Repository_Context,
+                                       Remote_Branch,
+                                       Remote_Commit_Uri)
+                ->  Remote_Commit_Uri_Option = some(Remote_Commit_Uri)
+                ;   Remote_Commit_Uri_Option = none
+                ),
+                branch_name_uri(Remote_Repository_Context, Remote_Branch, Remote_Branch_Uri)
+            ;   insert_branch_object(Remote_Repository_Context, Remote_Branch, Remote_Branch_Uri),
+                Remote_Commit_Uri_Option = none
             ),
-            branch_name_uri(Remote_Repository_Context, Remote_Branch, Remote_Branch_Uri)
-        ;   insert_branch_object(Remote_Repository_Context, Remote_Branch, Remote_Branch_Uri),
-            Remote_Commit_Uri_Option = none
-        ),
 
-        commit_id_uri(Repository_Context, Local_Commit_Id, Local_Commit_Uri),
-        (   Remote_Commit_Uri_Option = some(Remote_Commit_Uri),
-            commit_id_uri(Remote_Repository_Context, Remote_Commit_Id, Remote_Commit_Uri),
-            most_recent_common_ancestor(Repository_Context, Remote_Repository_Context, Local_Commit_Id,
-                                        Remote_Commit_Id, _Common_Commit_Id, _Local_Branch_Path, Remote_Branch_Path)
-        % Shared history
-        ->  (   Remote_Branch_Path = []
-            ->  true
-            ;   throw(error(remote_diverged(Remote_Branch_Path)))
+            commit_id_uri(Repository_Context, Local_Commit_Id, Local_Commit_Uri),
+            (   Remote_Commit_Uri_Option = some(Remote_Commit_Uri),
+                commit_id_uri(Remote_Repository_Context, Remote_Commit_Id, Remote_Commit_Uri),
+                most_recent_common_ancestor(Repository_Context, Remote_Repository_Context, Local_Commit_Id,
+                                            Remote_Commit_Id, _Common_Commit_Id, _Local_Branch_Path, Remote_Branch_Path)
+            % Shared history
+            ->  (   Remote_Branch_Path = []
+                ->  true
+                ;   throw(error(remote_diverged(Remote_Repository,Remote_Branch_Path)))
+                )
+            % No shared history
+            ;   true),
+
+            copy_commits(Repository_Context, Remote_Repository_Context, Local_Commit_Id),
+            reset_branch_head(Remote_Repository_Context, Remote_Branch_Uri, Local_Commit_Uri)
+        ;   (   has_branch(Remote_Repository_Context,
+                           Remote_Branch),
+                branch_head_commit(Remote_Repository_Context,
+                                   Remote_Branch, _)
+            ->  throw(error(remote_not_empty_on_local_empty(Remote_Repository)))
+            ;   insert_branch_object(Remote_Repository_Context, Remote_Branch, _)
             )
-        % No shared history
-        ;   true),
-
-        copy_commits(Repository_Context, Remote_Repository_Context, Local_Commit_Id),
-        branch_name_uri(Remote_Repository_Context, Remote_Branch, Remote_Branch_Uri),
-        reset_branch_head(Remote_Repository_Context, Remote_Branch_Uri, Local_Commit_Uri)
+        )
     ;   % Remote doesn't even exist yet
         throw(error(remote_does_not_exist(Remote_Repository)))
     ),
@@ -163,5 +169,91 @@ test(push_on_empty,
     resolve_absolute_string_descriptor("user/foo/remote/branch/master", Remote_Branch),
     findall(X-Y-Z, ask(Remote_Branch, t(X,Y,Z)), Triples),
     sort(Triples, [a-b-c,c-d-e]).
+
+
+test(push_twice,
+     [setup((setup_temp_store(State),
+             create_db_without_schema('user|foo','test','a test'))),
+      cleanup(teardown_temp_store(State))])
+:-
+    resolve_absolute_string_descriptor("user/foo", Descriptor),
+
+    Database_Descriptor = (Descriptor.repository_descriptor.database_descriptor),
+
+    resolve_relative_string_descriptor(Database_Descriptor, "remote/_commits", Repository_Descriptor),
+
+
+    create_context(Database_Descriptor, Database_Context),
+    with_transaction(Database_Context,
+                     insert_remote_repository(Database_Context, "remote", "http://somewhere", _),
+                     _),
+    create_ref_layer(Repository_Descriptor,
+                     _{doc : 'http://somewhere/', scm: 'http://somewhere/schema#'}),
+
+    create_context(Descriptor, commit_info{author: "me",
+                                           message: "something"},
+                   Branch_Context),
+
+    with_transaction(
+        Branch_Context,
+        ask(Branch_Context,
+            (   insert(a,b,c),
+                insert(c,d,e))),
+        _Meta_Data_B),
+
+    super_user_authority(Auth),
+
+    once(ask(Descriptor.repository_descriptor,
+                        t(_,layer:layer_id, Expected_Layer_Id^^(xsd:string)))),
+
+    push(Descriptor, "remote", "master", Auth, test_pusher(Expected_Layer_Id), _Result),
+
+    create_context(Descriptor, commit_info{author: "me",
+                                           message: "something else"},
+                   Branch_Context_2),
+
+    with_transaction(
+        Branch_Context_2,
+        ask(Branch_Context_2,
+            (   insert(h,i,j),
+                insert(k,l,m))),
+        _Meta_Data_B_2),
+
+
+    push(Descriptor, "remote", "master", Auth, test_pusher(_Expected_Layer_Id_2), _Result_2),
+
+    resolve_absolute_string_descriptor("user/foo/remote/branch/master", Remote_Branch),
+    findall(X-Y-Z, ask(Remote_Branch, t(X,Y,Z)), Triples),
+    sort(Triples, [a-b-c,c-d-e,h-i-j,k-l-m]).
+
+test(push_empty_branch,
+     [setup((setup_temp_store(State),
+             create_db_without_schema('user|foo','test','a test'))),
+      cleanup(teardown_temp_store(State))])
+:-
+    resolve_absolute_string_descriptor("user/foo", Descriptor),
+
+    Database_Descriptor = (Descriptor.repository_descriptor.database_descriptor),
+
+    resolve_relative_string_descriptor(Database_Descriptor, "remote/_commits", Remote_Repository_Descriptor),
+
+
+    create_context(Database_Descriptor, Database_Context),
+    with_transaction(Database_Context,
+                     insert_remote_repository(Database_Context, "remote", "http://somewhere", _),
+                     _),
+    create_ref_layer(Remote_Repository_Descriptor,
+                     _{doc : 'http://somewhere/', scm: 'http://somewhere/schema#'}),
+
+    super_user_authority(Auth),
+    writeq('testing 1'),
+    push(Descriptor, "remote", "master", Auth, test_pusher(_New_Layer_Id), _Result),
+    writeq('testing'),
+    has_branch(Remote_Repository_Descriptor, "master"),
+
+    true.
+
+
+
 
 :- end_tests(push).
