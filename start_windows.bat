f793a166bdd99fb1adbdd0433c155db2b9ca10eb
rrooij <robin@datachemist.com>
2020-07-24 17:09:45 +0200

    windows: fix typo

--- start_windows.bat
+++ start_windows.bat
@@ -5,7 +5,6 @@ set TERMINUSDB_SERVER_DB_PATH=%APPDATA%\TerminusDB\db
 set TERMINUSDB_SERVER_REGISTRY_PATH=%APPDATA%\TerminusDB\registry.pl
 set TERMINUSDB_SERVER_TMP_PATH=%APPDATA%\TerminusDB\tmp
 set TERMINUSDB_SERVER_INDEX_PATH=%APPDATA%\TerminusDB\index.html
-set TERMINUSDB_SERVER_PACK_DIR=%APPDATA%\
 set TERMINUSDB_LOG_PATH=%APPDATA%\TerminusDB\httpd.log
 set TERMINUSDB_SERVER_PACK_DIR=%ScriptDir%swipl\pack
 if not exist %TERMINUSDB_SERVER_DB_PATH% "%ScriptDir%swipl\bin\swipl" utils\db_init -s localhost -k root --autologin=true
