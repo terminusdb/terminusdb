cbdf5123da4069dc7f24fb581430393138bf5360
rrooij <robin@datachemist.com>
2020-08-05 15:47:39 +0200

    query: remove the predicates

--- core/query.pl
+++ core/query.pl
@@ -9,8 +9,6 @@
               askable_context/5,
               askable_settings_context/3,
               context_to_parent_transaction/2,
-              collection_descriptor_prefixes/2,
-              collection_descriptor_default_write_graph/2,
               context_extend_prefixes/3,
               context_default_prefixes/2,
               empty_context/1,
