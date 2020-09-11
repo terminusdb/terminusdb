ca44ac0e047f1eea924aea6f1fa7ccf3a131de00
rrooij <robin@datachemist.com>
2020-09-11 17:00:46 +0200

    remove ci scripts that are not needed anymore

--- .ci/trigger_appimage.sh
+++
@@ -1,5 +0,0 @@
-curl -u rrooij:$GITHUB_API_TOKEN \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/terminusdb-labs/terminusdb-appimage/actions/workflows/2394580/dispatches \
-  -d "{\"ref\":\"master\", \"inputs\": {\"terminusdb_branch\": \"$TRAVIS_BRANCH\"}}"
