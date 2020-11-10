6f0c516f29aba41d753957780db9d61c4d0ddda0
rrooij <robin@datachemist.com>
2020-11-10 12:15:33 +0100

    init_docker: use new store init instead of db_util

--- init_docker.sh
+++ init_docker.sh
@@ -7,9 +7,9 @@ TERMINUSDB_AUTOLOGIN=${TERMINUSDB_AUTOLOGIN:-false}
 TERMINUSDB_ENABLE_WELCOME_SCREEN=${TERMINUSDB_ENABLE_WELCOME_SCREEN:-false}
 
 if [ ! -d /app/terminusdb/storage/db ] && [ "$TERMINUSDB_ENABLE_WELCOME_SCREEN" = false ]; then
-    /app/terminusdb/utils/db_init -s "$TERMINUSDB_SERVER_NAME" -k "$TERMINUSDB_ADMIN_PASS" --port "$TERMINUSDB_SERVER_PORT" --autologin="$TERMINUSDB_AUTOLOGIN" --autoattach="$TERMINUSDB_AUTOATTACH"
+    /app/terminusdb/start.pl store init -s "$TERMINUSDB_SERVER_NAME" -k "$TERMINUSDB_ADMIN_PASS" --port "$TERMINUSDB_SERVER_PORT" --autologin="$TERMINUSDB_AUTOLOGIN" --autoattach="$TERMINUSDB_AUTOATTACH"
 elif [ "$TERMINUSDB_ENABLE_WELCOME_SCREEN" = false ]; then
-    /app/terminusdb/utils/db_init -s "$TERMINUSDB_SERVER_NAME" -k "$TERMINUSDB_ADMIN_PASS" --port "$TERMINUSDB_SERVER_PORT" --autologin="$TERMINUSDB_AUTOLOGIN" --autoattach="$TERMINUSDB_AUTOATTACH" --only-config
+    /app/terminusdb/start.pl store init -s "$TERMINUSDB_SERVER_NAME" -k "$TERMINUSDB_ADMIN_PASS" --port "$TERMINUSDB_SERVER_PORT" --autologin="$TERMINUSDB_AUTOLOGIN" --autoattach="$TERMINUSDB_AUTOATTACH" --only-config
 fi
 
 if [ "$TERMINUSDB_ENABLE_WELCOME_SCREEN" = true ]; then
