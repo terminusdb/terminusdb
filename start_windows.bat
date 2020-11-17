f5a1ed3658a1dac364c7788c3b0dee796b708f41
rrooij <robin@datachemist.com>
2020-11-17 15:56:42 +0100

    start_windows.bat: fix windows script for new CLI

--- start_windows.bat
+++ start_windows.bat
@@ -7,5 +7,5 @@ set TERMINUSDB_SERVER_TMP_PATH=%APPDATA%\TerminusDB\tmp
 set TERMINUSDB_SERVER_INDEX_PATH=%APPDATA%\TerminusDB\index.html
 set TERMINUSDB_LOG_PATH=%APPDATA%\TerminusDB\httpd.log
 set TERMINUSDB_SERVER_PACK_DIR=%ScriptDir%swipl\pack
-if not exist "%TERMINUSDB_SERVER_DB_PATH%" "%ScriptDir%swipl\bin\swipl" "%ScriptDir%utils\db_init" -s localhost -k root --autologin=true
-"%ScriptDir%swipl\bin\swipl.exe" "%ScriptDir%start.pl"
+if not exist "%TERMINUSDB_SERVER_DB_PATH%" "%ScriptDir%swipl\bin\swipl" "%ScriptDir%start.pl" "store init" "--server localhost" "--key root" --autologin=true
+"%ScriptDir%swipl\bin\swipl.exe" "%ScriptDir%start.pl" serve
