81a75c206148b1d0b790cf8de131015ac61a9dab
Gavin Mendel-Gleason <gavin@terminusdb.com>
2020-11-24 16:59:36 +0100

    Remove preamble

--- core/query/jsonld.pl
+++ core/query/jsonld.pl
@@ -16,23 +16,6 @@
  *
  * Definitions for translating and manipulating JSON_LD
  *
- * * * * * * * * * * * * * COPYRIGHT NOTICE  * * * * * * * * * * * * * * *
- *                                                                       *
- *  This file is part of TerminusDB.                                     *
- *                                                                       *
- *  TerminusDB is free software: you can redistribute it and/or modify   *
- *  it under the terms of the GNU General Public License as published by *
- *  the Free Software Foundation, under version 3 of the License.        *
- *                                                                       *
- *                                                                       *
- *  TerminusDB is distributed in the hope that it will be useful,        *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
- *  GNU General Public License for more details.                         *
- *                                                                       *
- *  You should have received a copy of the GNU General Public License    *
- *  along with TerminusDB.  If not, see <https://www.gnu.org/licenses/>. *
- *                                                                       *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 :- use_module(core(util)).
--- start.pl
+++ start.pl
@@ -1,20 +1,6 @@
 #!/usr/bin/env swipl
 
 /*
- *  This file is part of TerminusDB.
- *
- *  TerminusDB is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, under version 3 of the License, or
- *  (at your option) any later version.
- *
- *  TerminusDB is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with TerminusDB.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
