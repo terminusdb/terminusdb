aefe01414656a451df206c8a14f03b9d72e9fefe
Dmytri Kleiner <dk@trick.ca>
2020-11-27 17:44:33 +0100

    use SIGTERM to kill db

--- distribution/desktop/public/electron.js
+++ distribution/desktop/public/electron.js
@@ -23,7 +23,7 @@ electron.app.on('will-quit', (event) => {
           console.log('found', p.pid)
           console.log( 'PID: %s, COMMAND: %s, ARGUMENTS: %s',
             p.pid, p.command, p.arguments)
-          process.kill(p.pid)
+          process.kill(p.pid, 'SIGTERM')
         }
       })
       QUITTING = true
