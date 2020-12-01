be7788c2d0a125e608f27ad48082c746baf0b4e8
rrooij <robin@datachemist.com>
2020-12-01 12:01:35 +0100

    add rc to config

--- config/terminus_config.pl
+++ config/terminus_config.pl
@@ -63,7 +63,7 @@ jwt_public_key_id(Value) :-
     getenv_default('TERMINUSDB_SERVER_JWT_PUBLIC_KEY_ID', '', Value).
 
 console_base_url(Value) :-
-    getenv_default('TERMINUSDB_CONSOLE_BASE_URL', 'https://dl.bintray.com/terminusdb/terminusdb/canary', Value).
+    getenv_default('TERMINUSDB_CONSOLE_BASE_URL', 'https://dl.bintray.com/terminusdb/terminusdb/rc', Value).
 
 https_enabled :-
     getenv_default('TERMINUSDB_HTTPS_ENABLED', 'true', Value),
