b4fbd642b8e03a854106e8918d237c92a5d77388
rrooij <robin@datachemist.com>
2020-12-18 17:53:03 +0100

    increase console version to 4.1.0

--- config/terminus_config.pl
+++ config/terminus_config.pl
@@ -90,7 +90,7 @@ jwt_public_key_id(Value) :-
     getenv_default('TERMINUSDB_SERVER_JWT_PUBLIC_KEY_ID', '', Value).
 
 console_base_url(Value) :-
-    getenv_default('TERMINUSDB_CONSOLE_BASE_URL', 'https://dcm.ist/console/v4.0.0', Value).
+    getenv_default('TERMINUSDB_CONSOLE_BASE_URL', 'https://dcm.ist/console/v4.1.0', Value).
 
 https_enabled :-
     getenv_default('TERMINUSDB_HTTPS_ENABLED', 'true', Value),
