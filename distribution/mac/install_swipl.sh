4682152d9c0a5f88d0630dad5c2420728023840b
rrooij <robin@datachemist.com>
2021-01-13 18:05:30 +0100

    ci: try to cache mac os swipl

---
+++ distribution/mac/install_swipl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bash public/pack/terminus_store_prolog/.ci/install_swipl_dmg.sh
+cp -r /Applications/SWI-Prolog.app public/
--- distribution/mac/install_terminusdb_store_prolog.sh
+++ distribution/mac/install_terminusdb_store_prolog.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 if [[ $1 == "" ]]; then
     echo "Please add a terminus_store_prolog version number"
     exit 1
@@ -10,8 +9,5 @@ fi
 mkdir public/pack
 git clone --single-branch --branch "$2" https://github.com/terminusdb/terminusdb.git public/terminusdb-server
 git clone --single-branch --branch "$1" https://github.com/terminusdb/terminus_store_prolog.git public/pack/terminus_store_prolog
-bash public/pack/terminus_store_prolog/.ci/install_swipl_dmg.sh
-cp -r /Applications/SWI-Prolog.app public/
 mkdir -p public/pack/terminus_store_prolog/lib/x86_64-darwin
 curl -L "https://github.com/terminusdb/terminus_store_prolog/releases/download/$1/libterminus_store.dylib" > public/pack/terminus_store_prolog/lib/x86_64-darwin/libterminus_store.dylib
-npm run build-mac-os
