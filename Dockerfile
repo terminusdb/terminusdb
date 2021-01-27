4635fee18d15e9b61dffdf6a83e3fb89cd3a0faf
rrooij <robin@datachemist.com>
2021-01-27 14:03:57 +0100

    add proper tus version

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM terminusdb/terminus_store_prolog:v0.14.3
+ENV TUS_VERSION v0.0.3
 WORKDIR /app/pack
 RUN export BUILD_DEPS="git build-essential make libjwt-dev libssl-dev pkg-config" \
         && apt-get update && apt-get install $BUILD_DEPS -y --no-install-recommends \
         && git clone --single-branch --branch v0.0.3-fix-make https://github.com/terminusdb-labs/jwt_io.git jwt_io \
-        && git clone --single-branch --branch v0.0.2 https://github.com/terminusdb/tus.git tus \
+        && git clone --single-branch --branch $TUS_VERSION https://github.com/terminusdb/tus.git tus \
         && swipl -g "pack_install('file:///app/pack/jwt_io', [interactive(false)])" \
         && swipl -g "pack_install('file:///app/pack/tus', [interactive(false)])"
 
--- distribution/mac/install_dependencies.sh
+++ distribution/mac/install_dependencies.sh
@@ -6,9 +6,13 @@ if [[ $2 == "" ]]; then
     echo "Please add current branch"
     exit 1
 fi
+if [[ $3 == "" ]]; then
+    echo "Please add tus pack version number"
+    exit 1
+fi
 mkdir public/pack
 git clone --single-branch --branch "$2" https://github.com/terminusdb/terminusdb.git public/terminusdb-server
 git clone --single-branch --branch "$1" https://github.com/terminusdb/terminus_store_prolog.git public/pack/terminus_store_prolog
-git clone https://github.com/terminusdb/tus.git public/pack/tus
+git clone --single-branch --branch "$3" https://github.com/terminusdb/tus.git public/pack/tus
 mkdir -p public/pack/terminus_store_prolog/lib/x86_64-darwin
 curl -L "https://github.com/terminusdb/terminus_store_prolog/releases/download/$1/libterminus_store.dylib" > public/pack/terminus_store_prolog/lib/x86_64-darwin/libterminus_store.dylib
