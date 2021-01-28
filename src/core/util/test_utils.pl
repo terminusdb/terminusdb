7854e890e04179d1030c44e25f615f3e4cfe1fca
rrooij <robin@datachemist.com>
2021-01-28 11:16:07 +0100

    tests: don't require explicit env variable sets for JWT tests
    The tests should be self-contained and should not rely on a
    proper setup ENV environment.

--- src/core/util/test_utils.pl
+++ src/core/util/test_utils.pl
@@ -412,7 +412,7 @@ spawn_server_1(Path, URL, PID, Options) :-
     ),
 
     format(string(URL), "http://127.0.0.1:~d", [Port]),
-
+    expand_file_search_path(terminus_home('test/public_key_test.key.pub'), Pub_Key),
     Env_List_1 = [
         'LANG'='en_US.UTF-8',
         'LC_TIME'='en_US.UTF-8',
@@ -423,7 +423,9 @@ spawn_server_1(Path, URL, PID, Options) :-
 
         'TERMINUSDB_SERVER_PORT'=Port,
         'TERMINUSDB_SERVER_DB_PATH'=Path,
-        'TERMINUSDB_HTTPS_ENABLED'='false'
+        'TERMINUSDB_HTTPS_ENABLED'='false',
+        'TERMINUSDB_SERVER_JWT_PUBLIC_KEY_PATH'=Pub_Key,
+        'TERMINUSDB_SERVER_JWT_PUBLIC_KEY_ID'='testkey'
     ],
 
     inherit_env_vars(Env_List_1,
