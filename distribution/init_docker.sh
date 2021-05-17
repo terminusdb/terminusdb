493ac9598db419f0f34023e4a780786281ee4453
Donny Winston <donny@polyneme.xyz>
2021-05-17 16:12:24 -0400

    allow TERMINUSDB_ADMIN_PASS_FILE

--- distribution/init_docker.sh
+++ distribution/init_docker.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
-TERMINUSDB_ADMIN_PASS=${TERMINUSDB_ADMIN_PASS:-root}
 TERMINUSDB_SERVER_PORT=${TERMINUSDB_SERVER_PORT:-6363}
 TERMINUSDB_AUTOLOGIN=${TERMINUSDB_AUTOLOGIN:-false}
 TERMINUSDB_ENABLE_WELCOME_SCREEN=${TERMINUSDB_WELCOME_SCREEN:-false}
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+file_env 'TERMINUSDB_ADMIN_PASS'
+TERMINUSDB_ADMIN_PASS=${TERMINUSDB_ADMIN_PASS:-root}
 
 if [ ! -d /app/terminusdb/storage/db ] && [ "$TERMINUSDB_ENABLE_WELCOME_SCREEN" = false ]; then
     /app/terminusdb/terminusdb store init --key "$TERMINUSDB_ADMIN_PASS"
