e09cda3509cbdf46048eaba9547a10d341e1e407
Matthijs van Otterdijk <matthijs@terminusdb.com>
2021-06-16 12:45:22 +0200

    implement minimized json printing without newlines

--- src/server/routes.pl
+++ src/server/routes.pl
@@ -957,10 +957,10 @@ ensure_json_header_written(Request, Header_Written) :-
         format("Content-type: application/json; charset=UTF-8~n~n", [])
     ;   true).
 
-json_write_with_header(Request, Document, Header_Written) :-
+json_write_with_header(Request, Document, Header_Written, JSON_Options) :-
     ensure_json_header_written(Request, Header_Written),
 
-    json_write(current_output, Document),
+    json_write(current_output, Document, JSON_Options),
     nl.
 
 document_handler(get, Path, Request, System_DB, Auth) :-
@@ -982,15 +982,19 @@ document_handler(get, Path, Request, System_DB, Auth) :-
                   error(count_is_not_a_number(Count_Atom), _))
     ;   Count = unlimited),
 
+    (   memberchk(minimized=true, Search)
+    ->  JSON_Options = [width(0)]
+    ;   JSON_Options = []),
+
     Header_Written = written(_),
     (   memberchk(id=Id, Search)
     ->  api_get_document(System_DB, Auth, Path, Graph_Type, Id, Document),
-        json_write_with_header(Request, Document, Header_Written)
+        json_write_with_header(Request, Document, Header_Written, JSON_Options)
     ;   memberchk(type=Type, Search)
     ->  forall(api_generate_documents_by_type(System_DB, Auth, Path, Graph_Type, Type, Skip, Count, Document),
-               json_write_with_header(Request, Document, Header_Written))
+               json_write_with_header(Request, Document, Header_Written, JSON_Options))
     ;   forall(api_generate_documents(System_DB, Auth, Path, Graph_Type, Skip, Count, Document),
-               json_write_with_header(Request, Document, Header_Written))),
+               json_write_with_header(Request, Document, Header_Written, JSON_Options))),
 
     % ensure the header has been written by now.
     ensure_json_header_written(Request, Header_Written).
