9a87d5fb2330bff06cf474ece0083291403d510e
Matthijs van Otterdijk <matthijs@terminusdb.com>
2021-11-30 12:07:34 +0100

    more work

--- src/core/document/idgen.pl
+++ src/core/document/idgen.pl
@@ -10,6 +10,9 @@
 %%            | lexical(Type_Name, +Fields, -Suffix, -Id_Fragment)
 %%            | hash(Type_Name, +Fields, -Suffix, -Id_Fragment)
 %%            | valuehash(Type_Name, +Fields, -Suffix, -Id_Fragment)
+%%            | array_index(Index, Inner_Component, Id_Fragment)
+%%            | list_index(Index, Inner_Component, Id_Fragment)
+%%            | property(Property, Inner_Component, Id_Fragment)
 %%
 %% field: field(Type, Property, String_Val)
 %%        | child_field(field, Type, Property, String_Val)
@@ -36,20 +39,43 @@ resolve_document_id(prefix(_)) :-
     % we got here cause prefix was not ground, which is an error
     throw(error(prefix_unknown_while_generating_id, _)).
 
-resolve_document_id_component(random(_Base, Id_Fragment)) :-
-    % todo check that fragment actually matches type name maybe
-    ground(Id_Fragment),
-    !.
+resolve_document_id_propertied_component(Property, Inner_Component, Id_Fragment) :-
+    \+ ground(Inner_Component),
+    !,
+    resolve_document_id_component(Inner_Component),
+    resolve_document_id_propertied_component(Property, Inner_Component, Id_Fragment).
+resolve_document_id_propertied_component(Property, Inner_Component, Id_Fragment) :-
+    component_fragment(Inner_Component, Inner_Fragment),
+    format(string(Id_Fragment), "~s/~s", [Property, Inner_Fragment]).
+
+resolve_document_id_indexed_component(Index, Inner_Component, Id_Fragment) :-
+    \+ ground(Inner_Component),
+    !,
+    resolve_document_id_component(Inner_Component),
+    resolve_document_id_indexed_component(Index, Inner_Component, Id_Fragment).
+resolve_document_id_indexed_component(Index, Inner_Component, Id_Fragment) :-
+    component_fragment(Inner_Component, Inner_Fragment),
+    format(string(Id_Fragment), "~d/~s", [Index, Inner_Fragment]).
+
+resolve_document_id_component(property(Property, Inner_Component, Id_Fragment)) :-
+    resolve_document_id_propertied_component(Property, Inner_Component, Id_Fragment).
+resolve_document_id_component(array_index(Index, Inner_Component, Id_Fragment)) :-
+    resolve_document_id_indexed_component(Index, Inner_Component, Id_Fragment).
+resolve_document_id_component(list_index(Index, Inner_Component, Id_Fragment)) :-
+    resolve_document_id_indexed_component(Index, Inner_Component, Id_Fragment).
 resolve_document_id_component(random(Base, Id_Fragment)) :-
     random(X),
     format(string(S), '~w', [X]),
     crypto_data_hash(S, Hash, [algorithm(sha256)]),
-    format(string(Id_Fragment),'~w~w',[Base, Hash]).
+    format(string(Id_Fragment),'~w/~w',[Base, Hash]).
 
 component_fragment(random(_, Fragment), Fragment).
 component_fragment(lexical(_, _, _, Fragment), Fragment).
 component_fragment(hash(_, _, _, Fragment), Fragment).
 component_fragment(valuehash(_, _, _, Fragment), Fragment).
+component_fragment(array_index(_, _, Fragment), Fragment).
+component_fragment(list_index(_, _, Fragment), Fragment).
+component_fragment(property(_, _, Fragment), Fragment).
 
 combine_fragments(prefix(P), Component, Id) :-
     component_fragment(Component, Fragment),
