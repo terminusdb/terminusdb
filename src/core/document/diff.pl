12ffc7e567f93b4d152843be6653a4a6c08cb9f4
Gavin Mendel-Gleason <gavin@terminusdb.com>
2021-12-02 09:23:18 +0100

    List version (about to work on tree)

--- src/core/document/diff.pl
+++ src/core/document/diff.pl
@@ -1,65 +1,71 @@
 :- module('document/diff',
-          [patch/3,
-           diff/3]).
+          [patch_list/3,
+           diff_list/3]).
 
 :- use_module(core(util)).
 
 % example for lists:
 %
-% diff := end
-%       | cpy(Item,Diff)
-%       | del(Item,Diff)
-%       | ins(Item,Diff)
+% diff := end_list
+%       | cpy_list(Item,Diff)
+%       | del_list(Item,Diff)
+%       | ins_list(Item,Diff)
 %
-% Takes a pair of list of lists and obtains a diff.
 
-patch(ins(Item,Diff)) -->
-    patch(Diff),
-    insert(Item).
-patch(del(Item,Diff)) -->
-    delete(Item),
-    patch(Diff).
-patch(cpy(Item,Diff)) -->
-    delete(Item),
-    patch(Diff),
-    insert(Item).
-patch(end) --> [].
+patch_list(ins_list(Item,Diff)) -->
+    patch_list(Diff),
+    insert_list(Item).
+patch_list(del_list(Item,Diff)) -->
+    delete_list(Item),
+    patch_list(Diff).
+patch_list(cpy_list(Item,Diff)) -->
+    delete_list(Item),
+    patch_list(Diff),
+    insert_list(Item).
+patch_list(end_list) --> [].
 
-insert(Item, Xs, [Item|Xs]).
+insert_list(Item, Xs, [Item|Xs]).
 
-delete(Item, [Item|Xs], Xs).
+delete_list(Item, [Item|Xs], Xs).
 
-cost(ins(_,Diff), N) :-
-    cost(Diff, M),
+cost_list(ins_list(_,Diff), N) :-
+    cost_list(Diff, M),
     N is 1 + M.
-cost(del(_,Diff), N) :-
-    cost(Diff, M),
+cost_list(del_list(_,Diff), N) :-
+    cost_list(Diff, M),
     N is 1 + M.
-cost(cpy(_,Diff), N) :-
-    cost(Diff, M),
+cost_list(cpy_list(_,Diff), N) :-
+    cost_list(Diff, M),
     N is 1 + M.
-cost(end, 0).
+cost_list(end_list, 0).
 
-diff([], [], end).
-diff([], [Y|Ys], ins(Y,Zs)) :-
-    diff([], Ys, Zs).
-diff([X|Xs], [], del(X,Zs)) :-
-    diff(Xs, [], Zs).
-diff([X|Xs], [Y|Ys], Best) :-
-    diff(Xs, [Y|Ys], Diff1),
-    diff([X|Xs], Ys, Diff2),
+diff_list([], [], end_list).
+diff_list([], [Y|Ys], ins_list(Y,Zs)) :-
+    diff_list([], Ys, Zs).
+diff_list([X|Xs], [], del_list(X,Zs)) :-
+    diff_list(Xs, [], Zs).
+diff_list([X|Xs], [Y|Ys], Best) :-
+    diff_list(Xs, [Y|Ys], Diff1),
+    diff_list([X|Xs], Ys, Diff2),
     (   X = Y
-    ->  diff(Xs, Ys, Diff3),
-        min(del(X,Diff1), ins(X,Diff2), DiffI),
-        min(DiffI, cpy(X,Diff3), Best)
-    ;   min(del(X,Diff1), ins(X,Diff2), Best)
+    ->  diff_list(Xs, Ys, Diff3),
+        min_list(del_list(X,Diff1), ins_list(X,Diff2), DiffI),
+        min_list(DiffI, cpy_list(X,Diff3), Best)
+    ;   min_list(del_list(X,Diff1), ins_list(X,Diff2), Best)
     ).
 
-min(Diff1, Diff2, Diff) :-
-    cost(Diff1, Cost1),
-    cost(Diff2, Cost2),
+min_list(Diff1, Diff2, Diff) :-
+    cost_list(Diff1, Cost1),
+    cost_list(Diff2, Cost2),
     (   Cost1 =< Cost2
     ->  Diff = Diff1
     ;   Diff = Diff2).
 
 
+% example for trees:
+%
+% diff := end_tree
+%       | cpy_tree(Label,Index,Diff)
+%       | del_tree(Lable,Index,Item,Diff)
+%       | ins_list(Item,Diff)
+%
