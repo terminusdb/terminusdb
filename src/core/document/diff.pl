4c99e6211b69c7ae96b7dd91cadc7950e48187f3
Gavin Mendel-Gleason <gavin@terminusdb.com>
2021-12-05 11:05:13 +0100

    Add hash blocking, add cost directly to diff

--- src/core/document/diff.pl
+++ src/core/document/diff.pl
@@ -26,6 +26,25 @@ patch(swap(N1,M1,N2,M2,Old,New,Diff_TR,Diff_BL,Diff_BR), In, Out) :-
     split_matrix(Out, N2, M2, New, TR_New, BL_New, BR_New).
 patch(keep, T, T).
 
+diff(T1,T2, Diff) :-
+    diff(T1,T2,Diff,_).
+
+best_cost(State,Cost) :-
+    arg(1, State, Cost).
+
+best_diff(State,Diff) :-
+    arg(2, State, Diff).
+
+diff(T1,T2,Diff,Cost) :-
+    State = best(inf,keep),
+    (   diff_(T1,T2,New_Diff,0,New_Cost,State),
+        nb_setarg(1,State,New_Cost),
+        nb_setarg(2,State,New_Diff),
+        fail
+    ;   best_cost(State, Cost),
+        best_diff(State, Diff)
+    ).
+
 cost(copy(_,_,TR,BL,BR),Cost) :-
     cost(TR,TR_Cost),
     cost(BL,BL_Cost),
@@ -36,45 +55,45 @@ cost(swap(N1,M1,N2,M2,_,_,TR,BL,BR),Cost) :-
     cost(BL,BL_Cost),
     cost(BR,BR_Cost),
     Cost is N1 * M1 + N2 * M2 + TR_Cost + BL_Cost + BR_Cost + 1.
-cost(keep,1).
+cost(keep, 1).
 
-diff(T1,T2, Diff) :-
-    sfoldr(
-        infimum,
-        diff_(T1,T2),
-        infinity,
-        Diff).
+down_from(From,To,X) :-
+    between(To,From,Y),
+    X is From - Y.
 
-infimum(infinity, Diff, Diff).
-infimum(Diff, infinity, Diff).
-infimum(DiffX, DiffY, Diff) :-
-    cost(DiffX, CostX),
-    cost(DiffY, CostY),
-    (   CostX =< CostY
-    ->  Diff = DiffX
-    ;   Diff = DiffY).
-
-%:- table diff_/3 as private.
-diff_(M,M,keep) :-
-    !.
-diff_([],M2,swap(0,0,R2,C2,[],M2,keep,keep,keep)) :-
+diff_(M,M,keep,
+      Current_Cost,Cost,State) :-
+    !,
+    best_cost(State, Best_Cost),
+    Cost is Current_Cost + 1,
+    Cost < Best_Cost.
+diff_([],M2,swap(0,0,R2,C2,[],M2,keep,keep,keep),
+      Current_Cost,Cost,State) :-
     !,
     row_length(M2,R2),
-    column_length(M2,C2).
-diff_(M1,[],swap(R1,C1,0,0,M1,[],keep,keep,keep)) :-
+    column_length(M2,C2),
+    best_cost(State, Best_Cost),
+    Cost is Current_Cost + R2 * C2 + 4, % 1 for each op
+    Cost < Best_Cost.
+diff_(M1,[],swap(R1,C1,0,0,M1,[],keep,keep,keep),
+      Current_Cost,Cost,State) :-
     !,
     row_length(M1,R1),
-    column_length(M1,C1).
-diff_(T1,T2,Diff) :-
+    column_length(M1,C1),
+    best_cost(State, Best_Cost),
+    Cost is Current_Cost + R1 * C1 + 4, % 1 for each op
+    Cost < Best_Cost.
+diff_(T1,T2,Diff,
+      Current_Cost,Cost,State) :-
     row_length(T1,R1),
     column_length(T1,C1),
     row_length(T2,R2),
     column_length(T2,C2),
     !,
-    between(0,R1,I1),
-    between(0,R2,I2),
-    between(0,C1,J1),
-    between(0,C2,J2),
+    down_from(R1,0,I1),
+    down_from(R2,0,I2),
+    down_from(C1,0,J1),
+    down_from(C2,0,J2),
     % *Some* matrix gets smaller
     once((   Size1 is I1 * J1,
              Size1 >= 1
@@ -83,14 +102,19 @@ diff_(T1,T2,Diff) :-
     % the same section has advanced in both row and column
     split_matrix(T1, I1, J1, TL1, TR1, BL1, BR1),
     split_matrix(T2, I2, J2, TL2, TR2, BL2, BR2),
+    best_cost(State, Best_Cost),
     (   TL1 = TL2
-    ->  diff_(TR1,TR2,Diff_TR),
-        diff_(BL1,BL2,Diff_BL),
-        diff_(BR1,BR2,Diff_BR),
+    ->  New_Cost is Current_Cost + 1,
+        New_Cost < Best_Cost,
+        diff_(TR1,TR2,Diff_TR,New_Cost,Cost1,State),
+        diff_(BL1,BL2,Diff_BL,Cost1,Cost2,State),
+        diff_(BR1,BR2,Diff_BR,Cost2,Cost,State),
         Diff = copy(I1,J1,Diff_TR,Diff_BL,Diff_BR)
-    ;   diff_(TR1,TR2,Diff_TR),
-        diff_(BL1,BL2,Diff_BL),
-        diff_(BR1,BR2,Diff_BR),
+    ;   New_Cost is Current_Cost + I1 * J1 + I2 * J2 + 1,
+        New_Cost < Best_Cost,
+        diff_(TR1,TR2,Diff_TR,New_Cost,Cost1,State),
+        diff_(BL1,BL2,Diff_BL,Cost1,Cost2,State),
+        diff_(BR1,BR2,Diff_BR,Cost2,Cost,State),
         Diff = swap(I1, J1, I2, J2,
                     TL1,TL2,Diff_TR,Diff_BL,Diff_BR)
     ).
@@ -115,7 +139,6 @@ split(Index,List,Left,Right) :-
     length(Left, Index),
     append(Left,Right,List).
 
-%:- table split_matrix/7.
 split_matrix(In, N, M, Top_Left, Right, Bottom, Bottom_Right) :-
     when((   ground(In)
          ;   ground(Rows_Top),
@@ -158,6 +181,33 @@ matrix_fill([R|Rows], Max_Row, Max_Col, [NR|NRows]) :-
     append(R,Tail,NR),
     succ(Sub_Col, Max_Col),
     matrix_fill(Rows, Max_Row, Sub_Col, NRows).
+
+% Performs a "hash convolution" of the original matrix
+% by running a window across and hashing the window to produce
+% a new smaller matrix.
+hash_blocks(N,M,T1,T2) :-
+    row_length(T1,R),
+    column_length(T1,C),
+
+    findall(Columns,
+            (   between(0,R,I),
+                I =< R,
+                In is I + N,
+                In =< R,
+                findall(Print,
+                        (   between(0,C,J),
+                            J =< C,
+                            Jn is J + M,
+                            Jn =< C,
+                            split_matrix(T1,In,Jn,TL,_,_,_),
+                            split_matrix(TL,I,J,_,_,_,Finger),
+                            format(string(S), '~q', [Finger]),
+                            crypto_data_hash(S, Print, [algorithm(md5)])
+                        ),
+                        Columns),
+                \+ Columns = []
+            ),
+            T2).
 
 :- begin_tests(matrix).
 
@@ -287,9 +337,9 @@ test(diff_delete_col, []) :-
           [3,4]],
 
     diff(T1,T2,Diff),
+
     Diff = swap(2,1,2,0,[[o],
                          [o]],[],keep,keep,keep),
-
     patch(Diff,T1,T2).
 
 test(diff_delete_row, []) :-
@@ -359,9 +409,9 @@ test(diff_insert_simple_row_middle, [
     T1 = [ [ 1 ], [ 3 ] ],
     T2 = [ [ 1 ], [ 2 ], [ 3 ] ],
 
-    diff(T1,T2,Diff),
+    diff(T1,T2,Diff,Cost),
     Diff = swap(1,1,2,1,[[1]],[[1],[2]],keep,keep,keep),
-
+    cost(Diff,Cost),
     patch(Diff,T1,T2).
 
 test(diff_insert_row_middle, [
@@ -373,21 +423,21 @@ test(diff_insert_row_middle, [
            [ 4, 5, 6 ],
            [ 7, 8, 9 ] ],
 
-    diff(T1,T2,Diff),
-
+    diff(T1,T2,Diff,Cost),
     Diff = copy(1,3,keep,swap(0,3,1,3,
                               [],
                               [[4,5,6]],
                               keep,keep,keep),keep),
-
+    cost(Diff,Cost),
     patch(Diff,T1,T2).
 
 test(diff_shared_corner, []) :-
     M1 = [[a,b],
           [c,1]],
     M2 = [[1]],
-    diff(M1,M2, Diff),
+    diff(M1,M2, Diff, Cost),
     Diff = swap(2,2,1,1,[[a,b],[c,1]],[[1]],keep,keep,keep),
+    cost(Diff,Cost),
     patch(Diff, M1, M2).
 
 test(diff_shared_middle_row, []) :-
@@ -395,10 +445,16 @@ test(diff_shared_middle_row, []) :-
           [x,1,2,x],
           [o,o,o,o]],
     M2 = [[1,2]],
-    diff(M1,M2, Diff),
-    writeq(Diff).
+    diff(M1,M2, Diff, Cost),
+    % Not ideal
+    Diff = swap(3,4,1,2,[[a,b,d,3],[x,1,2,x],[o,o,o,o]],[[1,2]],
+                keep,keep,keep),
+    cost(Diff,Cost),
+    patch(Diff, M1, M2).
 
-test(diff_shared_middle, []) :-
+test(diff_shared_middle, [
+         blocked('heat death of the universe anyone?')
+     ]) :-
     T1 = [ [ o, o, o, o ],
            [ a, 1, 2, 3 ],
            [ b, 4, 5, 6 ],
@@ -409,28 +465,19 @@ test(diff_shared_middle, []) :-
            [ 4, 5, 6, "gar" ],
            [ 7, 8, 9, "gaz" ] ],
 
-    diff(T1,T2,Diff),
-    writeq(Diff).
-
+    diff(T1,T2,Diff,Cost),
+    writeq(Diff),
+    cost(Diff, Cost).
 
 test(diff_row_col_simple, []) :-
     T1 = [ [ 1, x ] ],
 
     T2 = [ [ "gaz" ],
            [ "gar" ] ],
-    diff(T1,T2,Diff),
-    writeq(Diff),
-    Diff = swap(1,2,2,1,[[1,x]],[["gaz"],["gar"]],
-                swap(1,0,2,0,[[]],[[],[]],
-                     keep,
-                     keep,
-                     keep),
-                keep,
-                keep),
-
-    patch(Diff, T1, X),
-    writeq(X).
-
+    diff(T1,T2,Diff,Cost),
+    Diff = swap(1,2,2,1,[[1,x]],[["gaz"],["gar"]],keep,keep,keep),
+    cost(Diff, Cost),
+    patch(Diff, T1, T2).
 
 test(diff_row_col, []) :-
     T1 = [ [ 1, 2, 3 ],
@@ -442,24 +489,41 @@ test(diff_row_col, []) :-
            [ 4, 5, 6, "gar" ],
            [ 7, 8, 9, "gaz" ] ],
 
-    diff(T1,T2,Diff),
-    writeq(Diff),
+    diff(T1,T2,Diff,Cost),
     Diff = copy(3,3,
-                swap(3,1,
-                     [[null],[null],[null]],
-                     [["goo"],["gar"],["gaz"]],
-                     keep,
-                     keep,
-                     keep),
-                swap(1,3,
-                     [[x,x,x]],
-                     [[null,null,null]],
-                     keep,
-                     keep,
-                     keep),
-                keep).
+                swap(0,0,3,1,[],[["goo"],["gar"],["gaz"]],
+                     keep,keep,keep),
+                swap(1,3,0,0,[[x,x,x]],[],keep,keep,keep),
+                keep),
+    cost(Diff, Cost),
+    patch(Diff, T1, T2).
+
+test(hash_shared_middle, [
+     ]) :-
+    T1 = [ [ o, o, o, o ],
+           [ a, 1, 2, 3 ],
+           [ b, 4, 5, 6 ],
+           [ c, 7, 8, 9 ],
+           [ d, x, x, x ] ],
+    hash_blocks(3,3,T1,Blocks1),
+
+    T2 = [ [ 1, 2, 3, "goo" ],
+           [ 4, 5, 6, "gar" ],
+           [ 7, 8, 9, "gaz" ] ],
+
+    hash_blocks(3,3,T2,Blocks2),
+
+    writeq(Blocks1),
+    writeq(Blocks2),
+
+    %diff(T1,T2,Diff,Cost),
+    %writeq(Diff),
+    %cost(Diff, Cost),
+    true.
 
 :- end_tests(matrix).
+
+
 
 /*
 T1 = [ [ 1, 2, 3 ],  |
