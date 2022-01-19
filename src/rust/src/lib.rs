0bbcc8ae232d58456c401de5d04fa593b0b49f56
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-01-19 13:17:02 +0100

    Add comment to make it clear that the hello native predicate is temporary

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,6 +1,9 @@
 use swipl::prelude::*;
 
 predicates! {
+    /// Temporary predicate to demonstrate and test the embedded
+    /// module. This should go away as soon as some real predicates
+    /// are added here.
     #[module("rustnative")]
     semidet fn hello(_context, term) {
         term.unify("Hello world")
