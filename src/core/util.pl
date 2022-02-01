a3bcfb7fc978b1e1ba32c42fc023d3bd149779d9
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-02-02 00:27:47 +0100

    Properly reset the captures when a transaction is rerun

--- src/core/util.pl
+++ src/core/util.pl
@@ -133,6 +133,7 @@
               duplicates/2,
               has_duplicates/2,
               index_list/2,
+              nb_thread_var_init/2,
               nb_thread_var/2,
               uri_encoded_string/3,
 
