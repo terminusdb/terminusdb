c0144a1e58480e58f00e4c94bf181c98b8fbc0a6
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-02-02 15:03:48 +0100

    add setup and cleanup code in integration tests, though it doesn't work for orgs

--- tests/test/organization.js
+++ tests/test/organization.js
@@ -24,13 +24,50 @@ describe('organization', function () {
     }
   })
 
-  it('succeeds adding unknown organization', async function () {
-    await organization
-      .add(agent, {
+  describe('with random organization name', function () {
+    let params
+    before(async function () {
+      params = {
         organization_name: util.randomString(),
-        user_name: agent.defaults().userName,
-      })
-      .then(organization.verifyAddSuccess)
+      }
+    })
+    after(async function () {
+      // deleting organizations is unfortunately broken right now.
+      /*
+      await organization
+        .del(agent, params)
+        .then(organization.verifyDelSuccess)
+      */
+    })
+
+    it('succeeds adding organization', async function () {
+      await organization
+        .add(agent, params)
+        .then(organization.verifyAddSuccess)
+    })
+  })
+
+  describe('with random organization name with pipe', function () {
+    let params
+    before(async function () {
+      params = {
+        organization_name: util.randomString() + '|pipe',
+      }
+    })
+    after(async function () {
+      // deleting organizations is unfortunately broken right now.
+      /*
+      await organization
+        .del(agent, params)
+        .then(organization.verifyDelSuccess)
+      */
+    })
+
+    it('succeeds adding organization', async function () {
+      await organization
+        .add(agent, params)
+        .then(organization.verifyAddSuccess)
+    })
   })
 
   it('fails adding unknown user', async function () {
