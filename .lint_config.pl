f52ba6604c220115052c70527649c73750ad6d77
Robin de Rooij <rderooij685@gmail.com>
2022-02-04 16:37:58 +0100

    add sp_card to ignore

--- .lint_config.pl
+++ .lint_config.pl
@@ -3,3 +3,4 @@ ignore_predicate("dateTimeStamp/11").
 ignore_predicate("time/8").
 ignore_predicate("dateTime/11").
 ignore_predicate("embellish_flag/3").
+ignore_predicate("sp_card/4"). % We probably need to add it to terminus-store docs
