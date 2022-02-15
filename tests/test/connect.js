ea7f08c2556ce08f5c9d2e967dd77ed71c56ade5
Sean Leather <sean@terminusdb.com>
2022-02-15 19:48:38 +0200

    Remove console.error()

--- tests/test/connect.js
+++ tests/test/connect.js
@@ -5,7 +5,6 @@ describe('connect', function () {
   it('passes with no auth', async function () {
     const agent = new Agent()
     const r = await agent.get('/api/')
-    console.error(r)
     expect(r.status).to.equal(200)
     expect(r.body).to.be.an('array').that.has.lengthOf(0)
   })
@@ -13,7 +12,6 @@ describe('connect', function () {
   it('passes with auth', async function () {
     const agent = new Agent().auth()
     const r = await agent.get('/api/')
-    console.error(r)
     expect(r.status).to.equal(200)
     expect(r.body).to.be.an('array').that.has.lengthOf(0)
   })
