c6933dc578dd24b1e8436299c7762069b1d972c9
Sean Leather <sean@terminusdb.com>
2022-03-31 12:18:55 +0200

    Raise version to 10.0.22

--- src/config/terminus_config.pl
+++ src/config/terminus_config.pl
@@ -39,7 +39,7 @@
 :- use_module(library(apply)).
 :- use_module(library(yall)).
 
-terminusdb_version('10.0.21').
+terminusdb_version('10.0.22').
 
 bootstrap_config_files :-
     initialize_system_ssl_certs.
