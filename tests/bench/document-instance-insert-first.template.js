f8e67f394803e25ac69f01fc9c663754af326f3c
Sean Leather <sean@terminusdb.com>
2022-04-01 15:54:30 +0200

    Print benchmark document insert response (#1080)
    For debugging when these operations fail during CI. See #1062.

--- tests/bench/document-instance-insert-first.template.js
+++ tests/bench/document-instance-insert-first.template.js
@@ -1,4 +1,4 @@
-const { Agent, db, document, endpoint } = require('../lib')
+const { Agent, db, document, endpoint, util } = require('../lib')
 
 const agent = new Agent().auth()
 const defaults = agent.defaults()
@@ -21,6 +21,10 @@ module.exports = (schema, instance) => {
     afterEach: async () => {
       // Delete the database.
       await db.del(agent, endpoint.db(defaults).path).then(db.verifyDeleteSuccess)
+      // Print the response body for debugging.
+      if (util.isNonEmptyObject(response.body)) {
+        console.error(JSON.stringify(response.body, null, 2))
+      }
       // Verify the response.
       document.verifyInsertSuccess(response)
     },
