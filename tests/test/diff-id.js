2e1ddbd184949176dea422b90480118c7b555a10
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-04-08 15:24:23 +0200

    Clean up tests

--- tests/test/diff-id.js
+++ tests/test/diff-id.js
@@ -222,31 +222,42 @@ describe('diff-id', function () {
       const class2 = util.randomString()
       await document
         .insert(agent, docPath, {
-          schema: [{ '@type': 'Class', '@id': class1, a: 'xsd:string', b : class2 },
-                   { '@type': 'Class', '@id': class2, c: 'xsd:integer', '@subdocument' : [],
-                     '@key' : { '@type' : 'Lexical', '@fields' : ['c'] }},
-                  ],
+          schema: [{ '@type': 'Class', '@id': class1, a: 'xsd:string', b: class2 },
+            {
+              '@type': 'Class',
+              '@id': class2,
+              c: 'xsd:integer',
+              '@subdocument': [],
+              '@key': { '@type': 'Lexical', '@fields': ['c'] },
+            },
+          ],
         })
         .then(document.verifyInsertSuccess)
       const r1 = await document
         .insert(agent, docPath, {
-          instance: { '@type': class1, a: 'pickles and eggs',
-                      b: { '@type' : class2,
-                           c : 3
-                         }
-                    },
+          instance: {
+            '@type': class1,
+            a: 'pickles and eggs',
+            b: {
+              '@type': class2,
+              c: 3,
+            },
+          },
         })
         .then(document.verifyInsertSuccess)
       const dv1 = r1.header['terminusdb-data-version']
       const [docId] = r1.body
       const r2 = await document
         .replace(agent, docPath, {
-          instance: { '@type': class1, a: 'pickles and eggs',
-                      '@id' : docId,
-                      b: { '@type' : class2,
-                           c : 4
-                         }
-                    },
+          instance: {
+            '@type': class1,
+            a: 'pickles and eggs',
+            '@id': docId,
+            b: {
+              '@type': class2,
+              c: 4,
+            },
+          },
         })
         .then(document.verifyInsertSuccess)
       const dv2 = r2.header['terminusdb-data-version']
@@ -256,10 +267,9 @@ describe('diff-id', function () {
       const r3 = await agent.post(path).send(
         {
           before_data_version: dv1,
-          after_data_version: dv2
+          after_data_version: dv2,
         })
       expect(r3.status).to.equal(200)
-
     })
 
     it('diff unchanged', async function () {
@@ -267,26 +277,25 @@ describe('diff-id', function () {
       const class2 = util.randomString()
       await document
         .insert(agent, docPath, {
-          schema: [{ '@type': 'Class', '@id': class1, a: 'xsd:string'},
-                   { '@type': 'Class', '@id': class2, b: 'xsd:string'}
-                  ],
+          schema: [{ '@type': 'Class', '@id': class1, a: 'xsd:string' },
+            { '@type': 'Class', '@id': class2, b: 'xsd:string' },
+          ],
         })
         .then(document.verifyInsertSuccess)
       const r1 = await document
         .insert(agent, docPath, {
-          instance: { '@type': class1, a: 'pickles and eggs'},
+          instance: { '@type': class1, a: 'pickles and eggs' },
         })
         .then(document.verifyInsertSuccess)
       const dv1 = r1.header['terminusdb-data-version']
-      const [docId] = r1.body
       const r2 = await document
         .insert(agent, docPath, {
           instance: { '@type': class2, b: 'frog legs' },
         })
         .then(document.verifyInsertSuccess)
       const dv2 = r2.header['terminusdb-data-version']
-      const [docId2_long] = r2.body
-      const docId2 = docId2_long.split('terminusdb:///data/')[1]
+      const [docId2Long] = r2.body
+      const docId2 = docId2Long.split('terminusdb:///data/')[1]
       const { path } = endpoint.versionDiff(agent.defaults())
 
       const r3 = await agent.post(path).send(
@@ -300,10 +309,10 @@ describe('diff-id', function () {
           '@insert': {
             '@id': docId2,
             '@type': class2,
-            b: 'frog legs'
+            b: 'frog legs',
           },
-          '@op': 'Insert'
-        }
+          '@op': 'Insert',
+        },
       ])
     })
   })
