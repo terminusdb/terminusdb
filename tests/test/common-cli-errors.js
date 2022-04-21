47f2fa7d5bc837e86af54c0f3c06d0d25fd9114f
Sean Leather <sean@terminusdb.com>
2022-04-21 13:14:19 +0200

    Use random storage subdirectory for CLI tests
    Currently, the CLI integration tests force initialize the store, which
    removes any existing default store (`./storage/db`). This may not be
    desirable if you are currently using that store for development.
    
    However, sometimes, the tests fail or are killed, and it is useful to
    remove any existing store when re-running the tests.
    
    This change uses the `TERMINUSDB_SERVER_DB_PATH` environment variable to
    set the store to a random subdirectory of `./storage`. We can therefore
    both avoid removing the default store and remove any previously created
    random subdirectory used for tests.
    
    Fixes #1085.

--- tests/test/common-cli-errors.js
+++ tests/test/common-cli-errors.js
@@ -5,11 +5,14 @@ const { util } = require('../lib')
 
 describe('common-cli-errors', function () {
   before(async function () {
-    await exec('./terminusdb.sh store init --force')
+    process.env.TERMINUSDB_SERVER_DB_PATH = `./storage/${util.randomString()}`
+    const r = await exec('./terminusdb.sh store init --force')
+    expect(r.stdout).to.match(/^Successfully initialised database/)
   })
 
   after(async function () {
-    await fs.rm('storage', { recursive: true })
+    await fs.rm(process.env.TERMINUSDB_SERVER_DB_PATH, { recursive: true })
+    delete process.env.TERMINUSDB_SERVER_DB_PATH
   })
 
   describe('fails for unknown database', function () {
