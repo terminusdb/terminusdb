c1dac52579324f380e2cef20687a23b91b2f7f2c
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-04-25 00:27:35 +0200

    make version description more flexible

--- src/core/api/api_patch.pl
+++ src/core/api/api_patch.pl
@@ -17,20 +17,25 @@ api_diff(_System_DB, _Auth, Before, After, Keep, Diff) :-
               error(unground_patch, _)),
     simple_diff(Before,After,Keep,Diff).
 
-document_from_version_header(Branch_Descriptor, Version_Header, Doc_Id, Document, Transaction,
-                             Map_In, Map_Out) :-
-    do_or_die(
-        read_data_version(Version_Header, data_version(branch, Commit_Id)),
-        error(bad_data_version(Version_Header), _)
-    ),
+coerce_to_commit(Commit_Or_Version, Commit_Id) :-
+    (   read_data_version(Commit_Or_Version, data_version(Type, Commit_Id))
+    ->  do_or_die(
+            (   branch = Type
+            ;   commit = Type),
+            error(bad_data_version(Commit_Or_Version), _)
+        )
+    ;   Commit_Id = Commit_Or_Version
+    ).
 
+document_from_commit(Branch_Descriptor, Commit_Id, Doc_Id, Document, Transaction,
+                     Map_In, Map_Out) :-
     resolve_relative_descriptor(Branch_Descriptor,
                                 ["commit", Commit_Id],
                                 Commit_Descriptor),
 
-    do_or_die(
-        open_descriptor(Commit_Descriptor, commit_info{}, Transaction, Map_In, Map_Out),
-        error(unresolvable_collection(Commit_Descriptor),_)),
+   do_or_die(
+       open_descriptor(Commit_Descriptor, commit_info{}, Transaction, Map_In, Map_Out),
+       error(unresolvable_collection(Commit_Descriptor),_)),
 
     get_document(Transaction, Doc_Id, Document).
 
@@ -38,15 +43,20 @@ document_from_version_header(Branch_Descriptor, Version_Header, Doc_Id, Document
 api_diff_id(System_DB, Auth, Path, Before_Version, After_Version, Doc_Id, Keep, Diff) :-
     resolve_descriptor_auth(read, System_DB, Auth, Path, instance, Branch_Descriptor),
 
-    document_from_version_header(Branch_Descriptor, Before_Version, Doc_Id, Before, _, [], Map),
-    document_from_version_header(Branch_Descriptor, After_Version, Doc_Id, After, _, Map, _),
+    coerce_to_commit(Before_Version, Before_Commit_Id),
+    coerce_to_commit(After_Version, After_Commit_Id),
+
+    document_from_commit(Branch_Descriptor, Before_Commit_Id, Doc_Id, Before, _, [], Map),
+    document_from_commit(Branch_Descriptor, After_Commit_Id, Doc_Id, After, _, Map, _),
 
     simple_diff(Before,After,Keep,Diff).
 
 api_diff_id_document(System_DB, Auth, Path, Before_Version, After_Document, Doc_Id, Keep, Diff) :-
     resolve_descriptor_auth(read, System_DB, Auth, Path, instance, Branch_Descriptor),
 
-    document_from_version_header(Branch_Descriptor, Before_Version, Doc_Id, Before, Transaction, [], _),
+    coerce_to_commit(Before_Version, Before_Commit_Id),
+
+    document_from_commit(Branch_Descriptor, Before_Commit_Id, Doc_Id, Before, Transaction, [], _),
 
     normalize_document(Transaction, After_Document, Normal_Document),
     simple_diff(Before,Normal_Document,Keep,Diff).
