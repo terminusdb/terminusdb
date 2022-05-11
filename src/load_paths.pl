9737942999568b252d99020ad8e9550a469a4a1a
Robin de Rooij <rderooij685@gmail.com>
2022-05-11 15:55:35 +0200

    Refactor load_paths

--- src/load_paths.pl
+++ src/load_paths.pl
@@ -5,20 +5,22 @@
 
 :- set_prolog_flag(terminusdb_monolithic_module, true).
 
-root_path(Path) :-
+
+% The top-level directory of the repository.
+top_level_directory(Path) :-
     prolog_load_context(file, File),
     relative_file_name(Path, File, '../').
 
 add_terminus_home_path :-
-    root_path(RootDir),
-    atom_concat(RootDir, 'src', SrcDir),
+    top_level_directory(TopDir),
+    directory_file_path(TopDir, 'src', SrcDir),
     asserta(user:file_search_path(terminus_home, SrcDir)).
 
 :- add_terminus_home_path.
 
 add_library_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/library',Lib),
+    directory_file_path(Dir,'library',Lib),
     asserta(user:file_search_path(library, Lib)).
 
 :- add_library_path.
@@ -27,28 +29,28 @@ add_library_path :-
 
 add_core_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/core',Core),
+    directory_file_path(Dir,'core',Core),
     asserta(user:file_search_path(core, Core)).
 
 :- add_core_path.
 
 add_server_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/server',Server),
+    directory_file_path(Dir,'server',Server),
     asserta(user:file_search_path(server, Server)).
 
 :- add_server_path.
 
 add_cli_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/cli',Core),
+    directory_file_path(Dir,'cli',Core),
     asserta(user:file_search_path(cli, Core)).
 
 :- add_cli_path.
 
 add_jwt_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/prolog_jwt/prolog',Library),
+    directory_file_path(Dir,'prolog_jwt/prolog',Library),
     asserta(user:file_search_path(library, Library)).
 
 :- add_jwt_path.
@@ -61,7 +63,7 @@ add_config_path :-
     asserta(user:file_search_path(config, App_Config)),
     % relative path
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/config',Config),
+    directory_file_path(Dir,'config',Config),
     asserta(user:file_search_path(config, Config)).
 
 :- add_config_path.
@@ -77,47 +79,47 @@ add_pack_path :-
 
 add_test_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/test',Config),
+    directory_file_path(Dir,'test',Config),
     asserta(user:file_search_path(test, Config)).
 
 :- add_test_path.
 
 add_plugin_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/plugins',Config),
+    directory_file_path(Dir,'plugins',Config),
     asserta(user:file_search_path(plugins, Config)).
 
 :- add_plugin_path.
 
 add_ontology_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/terminus-schema',Ontology),
+    directory_file_path(Dir,'terminus-schema',Ontology),
     asserta(user:file_search_path(ontology, Ontology)).
 
 :- add_ontology_path.
 
 add_template_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir,'/utils',Template),
+    directory_file_path(Dir,'utils',Template),
     asserta(user:file_search_path(template, Template)).
 
 :- add_template_path.
 
 add_rust_path :-
     user:file_search_path(terminus_home, Dir),
-    atom_concat(Dir, '/rust',Rust),
+    directory_file_path(Dir, 'rust',Rust),
     asserta(user:file_search_path(foreign, Rust)).
 
 :- add_rust_path.
 
 add_enterprise_path :-
-    root_path(Dir),
-    atom_concat(Dir, 'terminusdb-enterprise/prolog', Enterprise),
+    top_level_directory(Dir),
+    directory_file_path(Dir, 'terminusdb-enterprise/prolog', Enterprise),
     asserta(user:file_search_path(enterprise, Enterprise)).
 
 add_enterprise_test_path :-
-    root_path(Dir),
-    atom_concat(Dir, 'terminusdb-enterprise/test', Enterprise),
+    top_level_directory(Dir),
+    directory_file_path(Dir, 'terminusdb-enterprise/test', Enterprise),
     asserta(user:file_search_path(enterprise_test, Enterprise)).
 
 :- if(getenv("TERMINUSDB_ENTERPRISE", true)).
