d0e766b66cc72598a3e6f371260d4c59770e4052
Sean Leather <sean@terminusdb.com>
2022-05-17 15:49:18 +0200

    Remove writeq

--- src/core/document/diff.pl
+++ src/core/document/diff.pl
@@ -764,7 +764,6 @@ test(deep_list_patch, []) :-
     Before = json{ asdf: json{ bar: [json{ baz: 'quux' }] } },
     After = json{ asdf: json{ bar: [json{ baz: 'quuz' }] } },
     simple_diff(Before,After,Diff,[keep(json{})]),
-    writeq(Diff),
     simple_patch(Diff,Before,success(After),[]).
 
 :- end_tests(simple_diff).
