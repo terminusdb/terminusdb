6088bcf668872d0c302a36d22172af32b3f1df62
Sean Leather <sean@terminusdb.com>
2022-05-31 14:26:24 +0200

    Reorg tests, fix #1215

--- tests/test/cli-doc.js
+++ tests/test/cli-doc.js
@@ -27,58 +27,86 @@ describe('cli-doc', function () {
     delete process.env.TERMINUSDB_SERVER_DB_PATH
   })
 
-  it('passes schema insert, get, replace, delete', async function () {
-    this.timeout(30000)
-    const schema = { '@type': 'Class', '@id': util.randomString(), x: 'xsd:hexBinary' }
-    {
-      const r = await exec(`./terminusdb.sh doc insert ${dbSpec} --graph_type=schema --data='${JSON.stringify(schema)}'`)
-      expect(r.stdout).to.match(new RegExp(`^Documents inserted:\n 1: ${schema['@id']}`))
-    }
-    {
-      const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=schema`)
-      const docs = r.stdout.split('\n').filter((line) => line.length > 0).map(JSON.parse)
-      expect(docs[0]).to.deep.equal(util.defaultContext)
-      expect(docs[1]).to.deep.equal(schema)
-    }
-    schema.y = { '@type': 'Optional', '@class': 'xsd:base64Binary' }
-    {
-      const r = await exec(`./terminusdb.sh doc replace ${dbSpec} --graph_type=schema --data='${JSON.stringify(schema)}'`)
-      expect(r.stdout).to.match(new RegExp(`^Documents replaced:\n 1: ${schema['@id']}`))
-    }
-    {
-      const r = await exec(`./terminusdb.sh doc delete ${dbSpec} --graph_type=schema --id=${schema['@id']}`)
-      expect(r.stdout).to.match(new RegExp(`^Documents deleted:\n 1: ${schema['@id']}`))
-    }
-  })
+  describe('passes schema insert, get, replace, delete', function () {
+    const schema = { '@type': 'Class', negativeInteger: 'xsd:negativeInteger' }
 
-  it('passes instance insert, get, replace, delete', async function () {
-    this.timeout(40000)
-    const schema = { '@type': 'Class', '@id': util.randomString(), x: 'xsd:negativeInteger' }
-    {
-      const r = await exec(`./terminusdb.sh doc insert ${dbSpec} --graph_type=schema --data='${JSON.stringify(schema)}'`)
-      expect(r.stdout).to.match(new RegExp(`^Documents inserted:\n 1: ${schema['@id']}`))
-    }
-    const instance = { '@type': schema['@id'], '@id': `${schema['@id']}/0`, x: -88 }
-    {
-      const r = await exec(`./terminusdb.sh doc insert ${dbSpec} --graph_type=instance --data='${JSON.stringify(instance)}'`)
-      expect(r.stdout).to.match(new RegExp(`^Documents inserted:\n 1: terminusdb:///data/${instance['@id']}`))
-    }
-    instance.x = -255
-    {
-      const r = await exec(`./terminusdb.sh doc replace ${dbSpec} --graph_type=instance --data='${JSON.stringify(instance)}'`)
-      expect(r.stdout).to.match(new RegExp(`^Documents replaced:\n 1: terminusdb:///data/${instance['@id']}`))
-    }
-    {
-      const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=instance`)
-      expect(JSON.parse(r.stdout)).to.deep.equal(instance)
-    }
-    {
-      const r = await exec(`./terminusdb.sh doc delete ${dbSpec} --graph_type=instance --id=${instance['@id']}`)
-      expect(r.stdout).to.match(new RegExp(`^Documents deleted:\n 1: ${schema['@id']}`))
-    }
-    {
-      const r = await exec(`./terminusdb.sh doc delete ${dbSpec} --graph_type=schema --id=${schema['@id']}`)
-      expect(r.stdout).to.match(new RegExp(`^Documents deleted:\n 1: ${schema['@id']}`))
-    }
+    before(async function () {
+      this.timeout(50000)
+      schema['@id'] = util.randomString()
+      {
+        const r = await exec(`./terminusdb.sh doc insert ${dbSpec} --graph_type=schema --data='${JSON.stringify(schema)}'`)
+        expect(r.stdout).to.match(new RegExp(`^Documents inserted:\n 1: ${schema['@id']}`))
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=schema`)
+        const docs = r.stdout.split('\n').filter((line) => line.length > 0).map(JSON.parse)
+        expect(docs[0]).to.deep.equal(util.defaultContext)
+        expect(docs[1]).to.deep.equal(schema)
+      }
+      schema.hexBinary = { '@type': 'Optional', '@class': 'xsd:hexBinary' }
+      {
+        const r = await exec(`./terminusdb.sh doc replace ${dbSpec} --graph_type=schema --data='${JSON.stringify(schema)}'`)
+        expect(r.stdout).to.match(new RegExp(`^Documents replaced:\n 1: ${schema['@id']}`))
+      }
+    })
+
+    after(async function () {
+      {
+        const r = await exec(`./terminusdb.sh doc delete ${dbSpec} --graph_type=schema --id=${schema['@id']}`)
+        expect(r.stdout).to.match(new RegExp(`^Documents deleted:\n 1: ${schema['@id']}`))
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=schema`)
+        expect(JSON.parse(r.stdout)).to.deep.equal(util.defaultContext)
+      }
+    })
+
+    it('passes instance insert, get, replace, delete', async function () {
+      this.timeout(100000)
+      const instance = { '@type': schema['@id'], '@id': `${schema['@id']}/${util.randomString()}`, negativeInteger: -88 }
+      {
+        const r = await exec(`./terminusdb.sh doc insert ${dbSpec} --graph_type=instance --data='${JSON.stringify(instance)}'`)
+        expect(r.stdout).to.match(new RegExp(`^Documents inserted:\n 1: terminusdb:///data/${instance['@id']}`))
+      }
+      instance.negativeInteger = -255
+      {
+        const r = await exec(`./terminusdb.sh doc replace ${dbSpec} --graph_type=instance --data='${JSON.stringify(instance)}'`)
+        expect(r.stdout).to.match(new RegExp(`^Documents replaced:\n 1: terminusdb:///data/${instance['@id']}`))
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=instance`)
+        expect(JSON.parse(r.stdout)).to.deep.equal(instance)
+      }
+      instance.hexBinary = 'deadbeef'
+      {
+        const r = await exec(`./terminusdb.sh doc replace ${dbSpec} --graph_type=instance --create --data='${JSON.stringify(instance)}'`)
+        expect(r.stdout).to.match(new RegExp(`^Documents replaced:\n 1: terminusdb:///data/${instance['@id']}`))
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=instance`)
+        expect(JSON.parse(r.stdout)).to.deep.equal(instance)
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc delete ${dbSpec} --graph_type=instance --id=${instance['@id']}`)
+        expect(r.stdout).to.match(new RegExp(`^Documents deleted:\n 1: ${instance['@id']}`))
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=instance`)
+        expect(r.stdout).to.equal('')
+      }
+      instance['@id'] = `${schema['@id']}/${util.randomString()}`
+      {
+        const r = await exec(`./terminusdb.sh doc replace ${dbSpec} --graph_type=instance --create --data='${JSON.stringify(instance)}'`)
+        expect(r.stdout).to.match(new RegExp(`^Documents replaced:\n 1: terminusdb:///data/${instance['@id']}`))
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc get ${dbSpec} --graph_type=instance`)
+        expect(JSON.parse(r.stdout)).to.deep.equal(instance)
+      }
+      {
+        const r = await exec(`./terminusdb.sh doc delete ${dbSpec} --graph_type=instance --id=${instance['@id']}`)
+        expect(r.stdout).to.match(new RegExp(`^Documents deleted:\n 1: ${instance['@id']}`))
+      }
+    })
   })
 })
