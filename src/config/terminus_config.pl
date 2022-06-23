86ec2d2641081f3783a86460ba47ab22e0482910
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-06-23 11:40:13 +0200

    Update versions in ci.yml for 10.1.1 release (#1247)
    * Update versions in ci.yml for 10.1.1 release
    
    * Update versions
    
    Co-authored-by: matko <matko@users.noreply.github.com>

--- src/config/terminus_config.pl
+++ src/config/terminus_config.pl
@@ -39,7 +39,7 @@
 :- use_module(library(yall)).
 
 /* [[[cog import cog; cog.out(f"terminusdb_version('{CURRENT_REPO_VERSION}').") ]]] */
-terminusdb_version('10.1.1').
+terminusdb_version('10.1.2').
 /* [[[end]]] */
 
 bootstrap_config_files :-
