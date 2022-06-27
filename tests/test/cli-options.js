75387fa7a608a3789bd2c3a7484cda9eb04c5fbc
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-06-27 18:30:39 +0200

    Create store for tests

--- tests/test/cli-options.js
+++ tests/test/cli-options.js
@@ -1,17 +1,42 @@
+const fs = require('fs/promises')
 const exec = require('util').promisify(require('child_process').exec)
 const { expect } = require('chai')
+const { util } = require('../lib')
 
 describe('cli-options', function () {
-  it('nice error for bad switch', async function () {
-    const r = await exec('./terminusdb.sh db create admin/bar -g foo')
-    const errorPattern = /ERROR: The command line option "g" does not exist for the command "db create".*/
-    expect(r.stderr).to.match(new RegExp(errorPattern))
+
+  before(async function () {
+    this.timeout(30000)
+    process.env.TERMINUSDB_SERVER_DB_PATH = './storage/' + util.randomString()
+    {
+      const r = await exec('./terminusdb.sh store init --force')
+      expect(r.stdout).to.match(/^Successfully initialised database/)
+    }
+    dbSpec = `admin/${util.randomString()}`
+    {
+      const r = await exec(`./terminusdb.sh db create ${dbSpec}`)
+      expect(r.stdout).to.match(new RegExp(`^Database created: ${dbSpec}`))
+    }
   })
 
-  it('nice error for bad type', async function () {
-    const r = await exec('./terminusdb.sh db create admin/bar --public=foo')
-    const errorPattern = /ERROR: flag 'public': expected atom parsable as boolean, found 'foo'.*/
-    expect(r.stdout).to.match(new RegExp(errorPattern))
+  after(async function () {
+    const r = await exec(`./terminusdb.sh db delete ${dbSpec}`)
+    expect(r.stdout).to.match(new RegExp(`^Database deleted: ${dbSpec}`))
+    await fs.rm(process.env.TERMINUSDB_SERVER_DB_PATH, { recursive: true })
+    delete process.env.TERMINUSDB_SERVER_DB_PATH
   })
 
+  describe('command line options report correct errors', function () {
+    it('nice error for bad switch', async function () {
+      const r = await exec('./terminusdb.sh db create admin/bar -g foo')
+      const errorPattern = /ERROR: The command line option "g" does not exist for the command "db create".*/
+      expect(r.stderr).to.match(new RegExp(errorPattern))
+    })
+
+    it('nice error for bad type', async function () {
+      const r = await exec('./terminusdb.sh db create admin/bar --public=foo')
+      const errorPattern = /ERROR: flag 'public': expected atom parsable as boolean, found 'foo'.*/
+      expect(r.stdout).to.match(new RegExp(errorPattern))
+    })
+  })
 })
