8061f64b1560275bbba26bcf3ea403da091d45a3
Sean Leather <sean@terminusdb.com>
2022-06-30 01:54:32 +0200

    CLI bundle/unbundle tests (#1235)
    * Refactor tests to simplify writing them
    
    * Fix one `SchemaCheckFailure` to put the witness in a list
    * Verify success by default
    * Simplify verifying failure, not found, etc.
    * Use default path to avoid redundant or global variables
    * Put the paths, responses, and errors under tests/lib/api
    * Remove `agent.defaults()` and use object spread (`...`)
    
    * Add CLI bundle/unbundle tests
    
    Also add infrastructure for writing simpler CLI integration tests.
    
    Co-authored-by: Matthijs van Otterdijk <matthijs@terminusdb.com>
    Co-authored-by: Gavin Mendel-Gleason <gavin@terminusdb.com>

---
+++ tests/test/cli-bundle.js
@@ -0,0 +1,61 @@
+const { expect } = require('chai')
+
+const { Agent, Cli, util } = require('../lib')
+
+describe('cli-bundle', function () {
+  let agent
+  let cli
+
+  before(async function () {
+    agent = new Agent()
+    cli = new Cli({ debugCommand: true, debugOutput: true })
+    await cli.store.init()
+  })
+
+  after(async function () {
+    await cli.cleanup()
+  })
+
+  it('passes bundle, unbundle empty database', async function () {
+    await cli.db.create(agent)
+    await cli.bundle(agent)
+    await cli.unbundle(agent)
+    await cli.db.delete(agent)
+  })
+
+  it('passes bundle, unbundle database with doc', async function () {
+    this.timeout(20000)
+    await cli.db.create(agent)
+    const schema = { '@type': 'Class', '@id': util.randomString() }
+    await cli.doc.insert(agent, schema, { graphType: 'schema' })
+    await cli.bundle(agent)
+    await cli.unbundle(agent)
+    const r = await cli.doc.get(agent, { graphType: 'schema' })
+    expect(r[0]).to.deep.equal(util.defaultContext)
+    expect(r[1]).to.deep.equal(schema)
+    await cli.db.delete(agent)
+  })
+
+  describe('bundle, unbundle different empty database', function () {
+    before(async function () {
+      await cli.db.create(agent)
+    })
+
+    after(async function () {
+      await cli.db.delete(agent)
+    })
+
+    it('passes', async function () {
+      this.timeout(20000)
+      const schema = { '@type': 'Class', '@id': util.randomString() }
+      await cli.doc.insert(agent, schema, { graphType: 'schema' })
+      await cli.bundle(agent)
+      await cli.db.delete(agent)
+      await cli.db.create(agent)
+      await cli.unbundle(agent)
+      const r = await cli.doc.get(agent, { graphType: 'schema' })
+      expect(r[0]).to.deep.equal(util.defaultContext)
+      expect(r[1]).to.deep.equal(schema)
+    })
+  })
+})
