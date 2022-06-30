7cec112e8c7985a48c13617f61fdae028d3859d2
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-06-30 15:06:56 +0200

    Remove test which may depend on prolog version

--- tests/test/cli-options.js
+++ tests/test/cli-options.js
@@ -27,11 +27,12 @@ describe('cli-options', function () {
   })
 
   describe('command line options report correct errors', function () {
-    it('nice error for bad switch', async function () {
-      const r = await exec('./terminusdb.sh db create admin/bar -g foo')
-      const errorPattern = /ERROR: The command line option "g" does not exist for the command "db create".*/
-      expect(r.stderr).to.match(new RegExp(errorPattern))
-    })
+    // This may depend on prolog version!
+    // it('nice error for bad switch', async function () {
+    //  const r = await exec('./terminusdb.sh db create admin/bar -g foo')
+    //  const errorPattern = /ERROR: The command line option "g" does not exist for the command "db create".*/
+    //  expect(r.stderr).to.match(new RegExp(errorPattern))
+    // })
 
     it('nice error for bad type', async function () {
       const r = await exec('./terminusdb.sh db create admin/bar --public=foo')
