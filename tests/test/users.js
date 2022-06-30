6811500d922c8263b831719ea87c8430b7c5b517
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-06-30 12:46:39 +0200

    Adding roles

--- tests/test/users.js
+++ tests/test/users.js
@@ -9,4 +9,15 @@ describe('users', function () {
     expect(result.body).to.equal(`terminusdb://system/data/User/${userName}`)
     expect(result.status).to.equal(200)
   })
+
+  it('passes delete', async function () {
+    const agent = new Agent().auth()
+    const userName = util.randomString()
+    await agent.post(`/api/users/${userName}`)
+
+    const result = await agent.delete(`/api/users/${userName}`)
+    console.log(result.body)
+    expect(result.body).to.equal(`terminusdb://system/data/User/${userName}`)
+    expect(result.status).to.equal(200)
+  })
 })
