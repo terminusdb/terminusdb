ff29bbd1c634236e3c9fbbf35d148966c0e196ec
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-07-06 22:12:10 +0200

    Add remaining typeless scnearios

--- src/core/document/instance.pl
+++ src/core/document/instance.pl
@@ -807,21 +807,32 @@ referential_range_candidate(Validation_Object,O,P,Type) :-
 references_untyped_range(Validation_Object,S,P,O) :-
     instance_layer(Validation_Object, Instance),
     global_prefix_expand(rdf:type, Rdf_Type),
+    global_prefix_expand(sys:foreign_type, Foreign_Type),
     global_prefix_expand(rdf:nil, Rdf_Nil),
     % Shared dictionary for predicates would be handy here!
     distinct(S-P-O,
              (   triple_addition(Instance, S, P, node(O)),
                  %% valid typeless scenarios:
                  %% * P = rdf:type
+                 %% * P = sys:foreign_type
                  %% * O is rdf:nil
-                 %% * range(P) = C ∧ is_enum(C)
+                 %% * range(P) = C ∧ is_enum(C) ∨ is_foreign(C)
                  \+ (   atom_string(Rdf_Type,P)
+                    ;   atom_string(Foreign_Type, P)
                     ;   atom_string(Rdf_Nil,O)
                     ;   triple(Instance, S, Rdf_Type, node(C)),
-                        class_predicate_type(Validation_Object, C, P, Type),
-                        (   Type = set(enum(_,_))
-                        ;   Type = optional(enum(_,_))
-                        ;   Type = enum(_,_))),
+                        class_predicate_type(Validation_Object, C, P, Pre_Type),
+                        (   Pre_Type = set(E)
+                        ->  type_descriptor(Validation_Object, E, Type)
+                        ;   Pre_Type = optional(E)
+                        ->  type_descriptor(Validation_Object, E, Type)
+                        ;   Pre_Type = Type
+                        ),
+                        (   Type = enum(_,_)
+                        ->  true
+                        ;   Type = foreign(_)
+                        )
+                    ),
                  \+ triple(Instance, O, Rdf_Type, node(_))
              )).
 
