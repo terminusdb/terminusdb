2f9f11d37c6819f3e5f12a43dcea96022b145e49
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-07-17 19:25:19 +0200

    Add CLI tests

---
+++ tests/test/cli-db.js
@@ -0,0 +1,33 @@
+const exec = require('util').promisify(require('child_process').exec)
+const { expect } = require('chai')
+const { util } = require('../lib')
+
+describe('cli-db', function () {
+  it('lists a db', async function () {
+    const r1 = await exec('./terminusdb.sh db list')
+    expect(r1.stdout).to.match(/^TerminusDB\n/)
+  })
+
+  it('lists an existing db', async function () {
+    const db = util.randomString()
+    await exec(`./terminusdb.sh db create admin/${db}`)
+    const r1 = await exec(`./terminusdb.sh db list admin/${db}`)
+    expect(r1.stdout).to.match(new RegExp(`^TerminusDB\n│\n└── admin/${db}`))
+  })
+
+  it('lists two dbs', async function () {
+    const db1 = util.randomString()
+    const db2 = util.randomString()
+    await exec(`./terminusdb.sh db create admin/${db1}`)
+    await exec(`./terminusdb.sh db create admin/${db2}`)
+    const r1 = await exec(`./terminusdb.sh db list admin/${db1} admin/${db2}`)
+    expect(r1.stdout).to.match(/^TerminusDB/)
+  })
+
+  it('lists an existing db and branches', async function () {
+    const db = util.randomString()
+    await exec(`./terminusdb.sh db create admin/${db}`)
+    const r1 = await exec(`./terminusdb.sh db list admin/${db} --branches`)
+    expect(r1.stdout).to.match(new RegExp(`^TerminusDB\n│\n└── admin/${db}\n    └── main`))
+  })
+})
