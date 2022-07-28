d6e7b7836e008b3f77c7ef576ef1df1962a06b50
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-07-28 10:21:37 +0200

    Additional tests for public / schema

--- tests/test/cli-db.js
+++ tests/test/cli-db.js
@@ -28,6 +28,7 @@ describe('cli-db', function () {
     await exec(`./terminusdb.sh db create admin/${db}`)
     const r1 = await exec(`./terminusdb.sh db list admin/${db}`)
     expect(r1.stdout).to.match(new RegExp(`^TerminusDB\n│\n└── admin/${db}`))
+    await exec(`./terminusdb.sh db delete admin/${db}`)
   })
 
   it('lists two dbs', async function () {
@@ -37,6 +38,8 @@ describe('cli-db', function () {
     await exec(`./terminusdb.sh db create admin/${db2}`)
     const r1 = await exec(`./terminusdb.sh db list admin/${db1} admin/${db2}`)
     expect(r1.stdout).to.match(/^TerminusDB/)
+    await exec(`./terminusdb.sh db delete admin/${db1}`)
+    await exec(`./terminusdb.sh db delete admin/${db2}`)
   })
 
   it('lists an existing db and branches', async function () {
@@ -44,6 +47,7 @@ describe('cli-db', function () {
     await exec(`./terminusdb.sh db create admin/${db}`)
     const r1 = await exec(`./terminusdb.sh db list admin/${db} --branches`)
     expect(r1.stdout).to.match(new RegExp(`^TerminusDB\n│\n└── admin/${db}\n    └── main`))
+    await exec(`./terminusdb.sh db delete admin/${db}`)
   })
 
   it('updates name of an existing db', async function () {
@@ -55,5 +59,42 @@ describe('cli-db', function () {
     const dbObjs = JSON.parse(r2.stdout)
     expect(dbObjs[0].label).to.equal('goo')
     expect(dbObjs[0].comment).to.equal('gah')
+    await exec(`./terminusdb.sh db delete admin/${db}`)
   })
+
+  it('puts database in schema free mode and returns it', async function () {
+    const db = util.randomString()
+    await exec(`./terminusdb.sh db create admin/${db}`)
+    // turn off schema
+    await exec(`./terminusdb.sh db update admin/${db} --schema false`)
+    // demonstrate success
+    const r3 = await exec(`./terminusdb.sh query admin/${db}/local/branch/main "t('terminusdb://data/Schema', rdf:type, X, schema)"`)
+    expect(r3.stdout).to.match(/^X\nrdf:nil/)
+    await exec(`./terminusdb.sh db update admin/${db} --schema true`)
+    // demonstrate success
+    const r4 = await exec(`./terminusdb.sh query admin/${db}/local/branch/main "t('terminusdb://data/Schema', rdf:type, X, schema)"`)
+    expect(r4.stdout).to.match(/^X\n$/)
+    await exec(`./terminusdb.sh db delete admin/${db}`)
+  })
+
+  it('puts database a database in public and returns it', async function () {
+    const db = util.randomString()
+    await exec(`./terminusdb.sh db create admin/${db}`)
+    // turn off schema
+    await exec(`./terminusdb.sh db update admin/${db} --public true`)
+    const q = `./terminusdb.sh query _system "
+        t(DB_Uri, name, \"${db}\"^^xsd:string),
+        t(Cap_Id, scope, DB_Uri),
+        t(Cap_Id, role, 'Role/consumer'),
+        t('User/anonymous', capability, Cap_Id)"`
+    // demonstrate success
+    const r3 = await exec(q)
+    expect(r3.stdout).to.match(/^DB_Uri\s+Cap_Id\n.*\n$/)
+    await exec(`./terminusdb.sh db update admin/${db} --public false`)
+    // demonstrate success
+    const r4 = await exec(q)
+    expect(r4.stdout).to.match(/^DB_Uri\s+Cap_Id\n$/)
+    await exec(`./terminusdb.sh db delete admin/${db}`)
+  })
+
 })
