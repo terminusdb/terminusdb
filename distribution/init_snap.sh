50cf765a2a175dbdc1dfb8704617e7d0b6675137
Robin de Rooij <rderooij685@gmail.com>
2022-08-05 12:24:38 +0200

    init_snap.sh: add LD_LIBRARY_PATH to runtime

--- distribution/init_snap.sh
+++ distribution/init_snap.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-SWI_HOME_DIR="$SNAP/usr/lib/swi-prolog" $SNAP/terminusdb $@
+export SWI_HOME_DIR="$SNAP/usr/lib/swi-prolog"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}${SWI_HOME_DIR}/lib/x86_64-linux/"
+$SNAP/terminusdb $@
