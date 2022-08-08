39d426002c181cc6fdb6d0c2b9755a1ad55e5a3e
Robin de Rooij <rderooij685@gmail.com>
2022-08-08 11:51:16 +0200

    Fix quoting issue around snap wrapper

--- distribution/init_snap.sh
+++ distribution/init_snap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export SWI_HOME_DIR="$SNAP/usr/lib/swi-prolog"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}${SWI_HOME_DIR}/lib/x86_64-linux/"
-$SNAP/terminusdb $@
+$SNAP/terminusdb "$@"
