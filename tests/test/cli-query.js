15fc9f9319e8d54a7fe859fc0f02274253d6ae7c
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-11 14:01:30 +0200

    Catching errors from CLI

--- tests/test/cli-query.js
+++ tests/test/cli-query.js
@@ -22,4 +22,16 @@ describe('cli-query', function () {
     const r = await exec('./terminusdb.sh query _system \'split("A,B,C"^^xsd:string,","^^xsd:string,R)\'')
     expect(r.stdout).to.match(/["A"^^'xsd:string',"B"^^'xsd:string',"C"^^'xsd:string']/)
   })
+
+  it('errors on non-existent branch', async function () {
+    const db = util.randomString()
+    const r = await exec(`./terminusdb.sh query admin/${db} 'sum(1,2)' | true`)
+    expect(r.stderr).to.match(new RegExp(`^Error: The following descriptor could not be resolved to a resource: 'admin/${db}/local/branch/main'`))
+  })
+
+  it('catches existence error', async function () {
+    const r = await exec(`./terminusdb.sh query _system 'sum(1,2)' | true`)
+    expect(r.stderr).to.match(/^Error: The program: .* used a predicate with unhandled arguments/)
+  })
+
 })
