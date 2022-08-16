117ac69956b1ff017222213147ffa0220a120748
rrooij <rrooij@users.noreply.github.com>
2022-08-16 09:07:27 +0000

    Update versions

--- src/config/terminus_config.pl
+++ src/config/terminus_config.pl
@@ -42,7 +42,7 @@
 :- use_module(library(yall)).
 
 /* [[[cog import cog; cog.out(f"terminusdb_version('{CURRENT_REPO_VERSION}').") ]]] */
-terminusdb_version('10.1.2').
+terminusdb_version('10.1.4').
 /* [[[end]]] */
 
 bootstrap_config_files :-
