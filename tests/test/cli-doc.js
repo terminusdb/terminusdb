b09fd936c402296efec1569709eab843739589bc
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-18 22:08:43 +0200

    fix linting

--- tests/test/cli-doc.js
+++ tests/test/cli-doc.js
@@ -157,18 +157,19 @@ describe('cli-doc', function () {
     })
   })
 
-  describe('schema manipulation', function (){
-
+  describe('schema manipulation', function () {
     it('adds a bad language', async function () {
-      const schema = { '@base': 'terminusdb:///data/',
-                       '@schema': 'terminusdb:///schema#',
-                       '@type': '@context',
-                       '@documentation' : {
-                         '@language' : 'bogus',
-                         '@title' : 'Example Schema',
-                         '@description' : 'This is an example schema. We are using it to demonstrate the ability to display information in multiple languages about the same semantic content.',
-                         '@authors' : ['Gavin Mendel-Gleason']
-                       }}
+      const schema = {
+        '@base': 'terminusdb:///data/',
+        '@schema': 'terminusdb:///schema#',
+        '@type': '@context',
+        '@documentation': {
+          '@language': 'bogus',
+          '@title': 'Example Schema',
+          '@description': 'This is an example schema. We are using it to demonstrate the ability to display information in multiple languages about the same semantic content.',
+          '@authors': ['Gavin Mendel-Gleason'],
+        },
+      }
       const db = util.randomString()
       await exec(`./terminusdb.sh db create admin/${db}`)
       const r = await exec(`./terminusdb.sh doc insert -g schema admin/${db} --full-replace --data='${JSON.stringify(schema)}' | true`)
