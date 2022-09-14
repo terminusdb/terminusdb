2f6370a5f6d98fd70b283d7b7eac22cd422590c0
Gavin Mendel-Gleason <jacobian@gmail.com>
2022-09-14 16:57:15 +0200

    Adding lazy docs

---
+++ src/core/util/lazy_docs.pl
@@ -0,0 +1,23 @@
+:- module('util/lazy_docs', [stream_to_lazy_docs/2]).
+
+:- use_module(library(http/json)).
+
+% Maybe do this?
+buffer_size(100).
+
+stream_to_lazy_docs(Stream, List) :-
+    put_attr(List, lazy_docs, lazy_input(Stream)).
+
+attr_unify_hook(State, Value) :-
+    State = lazy_input(Stream),
+    (   json_read_dict(Stream, Term, [default_tag(json), end_of_file(eof)])
+    ->  (   Term = eof
+        ->  Value = []
+        ;   is_list(Term)
+        ->  stream_to_lazy_docs(Stream, Rest),
+            append(Term,Rest,Value)
+        ;   stream_to_lazy_docs(Stream, Rest),
+            Value = [Term|Rest]
+        )
+    ;   Value = []
+    ).
