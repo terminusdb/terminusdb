628220e392f08715e02824e1b5e95ad88b2fdc60
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-09-16 15:43:19 +0200

    change lazy doc logic to have the attr unify hook unify on head

--- src/core/util/lazy_docs.pl
+++ src/core/util/lazy_docs.pl
@@ -8,23 +8,23 @@ stream_to_lazy_docs(Stream, List) :-
     do_or_die(
         ground(Stream),
         error(instantiation_error, _)),
-    put_attr(List, 'util/lazy_docs', lazy_input(Stream, _)).
+    put_attr(List, 'util/lazy_docs', lazy_input(Stream, peek(_))).
 
-attr_unify_hook(State, Value) :-
-    State = lazy_input(Stream, Peek),
+attr_unify_hook(lazy_input(Stream, Peek_State), Value) :-
+    Peek_State = peek(Peek),
     (   var(Peek)
     ->  json_read_dict(Stream, Term, [default_tag(json), end_of_file(eof)]),
         (   Term = eof
-        ->  nb_setarg(2, State, []),
+        ->  nb_setarg(1, Peek_State, []),
             Value = []
         ;   is_list(Term)
         ->  stream_to_lazy_docs(Stream, Rest),
             append(Term,Rest,Result),
-            nb_linkarg(2, State, Result),
+            nb_linkarg(1, Peek_State, Result),
             Value = Result
         ;   stream_to_lazy_docs(Stream, Rest),
             Result = [Term|Rest],
-            nb_linkarg(2, State, Result),
+            nb_linkarg(1, Peek_State, Result),
             Value = Result
         )
     ;   Value = Peek
