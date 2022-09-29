a9b813024f5f41f3f616e41c5bf29992a1e9b2d2
Matthijs van Otterdijk <matthijs@datachemist.com>
2022-09-29 17:15:53 +0200

    serializing of fields into a more pretty struct

--- src/rust/terminusdb-community/src/graphql/frame.rs
+++ src/rust/terminusdb-community/src/graphql/frame.rs
@@ -36,82 +36,109 @@ fn default_dimensionality() -> usize { 1 }
 
 #[derive(Deserialize, PartialEq, Debug)]
 #[serde(tag = "@type")]
-enum ComplexFieldDefinition {
-    Optional{#[serde(rename = "@class")]class: InnerFieldDefinition},
-    Set{#[serde(rename = "@class")]class: InnerFieldDefinition},
-    Array{#[serde(rename = "@class")]class: InnerFieldDefinition, #[serde(default = "default_dimensionality")] dimensions: usize},
-    List{#[serde(rename = "@class")]class: InnerFieldDefinition},
-    Cardinality{#[serde(rename = "@class")]class: InnerFieldDefinition, min: Option<usize>, max: Option<usize>}
+enum StructuralComplexFieldDefinition {
+    Optional{#[serde(rename = "@class")]class: StructuralInnerFieldDefinition},
+    Set{#[serde(rename = "@class")]class: StructuralInnerFieldDefinition},
+    Array{#[serde(rename = "@class")]class: StructuralInnerFieldDefinition, #[serde(default = "default_dimensionality")] dimensions: usize},
+    List{#[serde(rename = "@class")]class: StructuralInnerFieldDefinition},
+    Cardinality{#[serde(rename = "@class")]class: StructuralInnerFieldDefinition, min: Option<usize>, max: Option<usize>},
+    Enum{#[serde(rename = "@id")] id: String, values: Vec<String>}
+
 }
 
 #[derive(Deserialize, PartialEq, Debug)]
-struct SubdocumentFieldDefinition {
+struct StructuralSubdocumentFieldDefinition {
     #[serde(rename = "@class")]
     class: String
 }
 
 #[derive(Deserialize, PartialEq, Debug)]
 #[serde(untagged)]
-enum InnerFieldDefinition {
+enum StructuralInnerFieldDefinition {
     SimpleField(String),
-    SubdocumentField(SubdocumentFieldDefinition)
+    Enum(StructuralEnumFieldDefinition),
+    SubdocumentField(StructuralSubdocumentFieldDefinition)
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+struct StructuralEnumFieldDefinition {
+    #[serde(rename = "@type")]
+    typ: String,
+    id: String,
+    values: Vec<String>
+}
+
+impl From<StructuralInnerFieldDefinition> for SimpleFieldDefinition {
+    fn from(f: StructuralInnerFieldDefinition) -> Self {
+        match f {
+            StructuralInnerFieldDefinition::SimpleField(s) => {
+                if is_base_type(&s) {
+                    SimpleFieldDefinition::BaseType(s)
+                }
+                else {
+                    SimpleFieldDefinition::Document{typ: s, is_subdocument: false}
+                }
+            },
+            StructuralInnerFieldDefinition::Enum(StructuralEnumFieldDefinition {id, values, ..}) =>
+                SimpleFieldDefinition::Enum{name: id, values},
+            StructuralInnerFieldDefinition::SubdocumentField(s) =>
+                    SimpleFieldDefinition::Document{typ: s.class, is_subdocument: true},
+        }
+    }
 }
 
 #[derive(Deserialize, PartialEq, Debug)]
 #[serde(untagged)]
-enum FieldDefinition {
+enum StructuralFieldDefinition {
     SimpleField(String),
-    ContainerField(ComplexFieldDefinition),
-    SubdocumentField(SubdocumentFieldDefinition)
+    ContainerField(StructuralComplexFieldDefinition),
+    SubdocumentField(StructuralSubdocumentFieldDefinition)
 }
 
-/*
 #[derive(PartialEq, Debug)]
+enum SimpleFieldDefinition {
+    BaseType(String),
+    Document{typ: String, is_subdocument: bool},
+    Enum{name: String, values: Vec<String>},
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+#[serde(from = "StructuralFieldDefinition")]
 enum FieldDefinition {
-    Required(FieldContentDefinition),
-    Optional(FieldContentDefinition),
-    Set(FieldContentDefinition),
-    Array{field: FieldContentDefinition, dimensions: usize},
-    List(FieldContentDefinition),
-    Cardinality{field: FieldContentDefinition, min: Option<usize>, max: Option<usize>}
-}
-*/
+    Required(SimpleFieldDefinition),
+    Optional(SimpleFieldDefinition),
+    Set(SimpleFieldDefinition),
+    List(SimpleFieldDefinition),
 
-/*
-
-impl<'de> Deserialize<'de> for FieldDefinition {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de> {
-        let visitor = FieldDefinitionVisitor;
-
-        deserializer.deserialize_any(visitor)
-    }
+    Array{field: SimpleFieldDefinition, dimensions: usize},
+    Cardinality{field: SimpleFieldDefinition, min: Option<usize>, max: Option<usize>},
 }
 
-struct FieldDefinitionVisitor;
+fn is_base_type(_s: &str) -> bool { true }
 
-impl<'de> Visitor<'de> for FieldDefinitionVisitor {
-    type Value = FieldDefinition;
+impl From<StructuralFieldDefinition> for FieldDefinition {
+    fn from(f: StructuralFieldDefinition) -> Self {
+        match f {
+            StructuralFieldDefinition::SimpleField(s) => {
+                if is_base_type(&s) {
+                    FieldDefinition::Required(SimpleFieldDefinition::BaseType(s))
+                }
+                else {
+                    FieldDefinition::Required(SimpleFieldDefinition::Document{typ: s, is_subdocument: false})
+                }
+            },
 
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(formatter, "a field definition")
-    }
+            StructuralFieldDefinition::ContainerField(StructuralComplexFieldDefinition::Optional{class}) => FieldDefinition::Optional(class.into()),
+            StructuralFieldDefinition::ContainerField(StructuralComplexFieldDefinition::Set{class}) => FieldDefinition::Set(class.into()),
+            StructuralFieldDefinition::ContainerField(StructuralComplexFieldDefinition::List{class}) => FieldDefinition::List(class.into()),
+            StructuralFieldDefinition::ContainerField(StructuralComplexFieldDefinition::Array{class, dimensions}) => FieldDefinition::Array{field: class.into(), dimensions},
+            StructuralFieldDefinition::ContainerField(StructuralComplexFieldDefinition::Cardinality{class, min, max}) => FieldDefinition::Cardinality{field: class.into(), min, max},
+            StructuralFieldDefinition::ContainerField(StructuralComplexFieldDefinition::Enum{id, values}) => FieldDefinition::Required(SimpleFieldDefinition::Enum{name: id, values}),
 
-    fn visit_string<E>(self, s: String) -> Result<FieldDefinition, E> {
-        Ok(FieldDefinition::Required(FieldContentDefinition::Direct(s)))
-    }
-    fn visit_str<E>(self, s: &str) -> Result<FieldDefinition, E> {
-        Ok(FieldDefinition::Required(FieldContentDefinition::Direct(s.to_string())))
-    }
-
-    fn visit_map<A:MapAccess<'de>>(self, mut map: A) -> Result<FieldDefinition, E> {
-        while let Some(key) = map.next_key::<Atom>()? {
-
+            StructuralFieldDefinition::SubdocumentField(StructuralSubdocumentFieldDefinition{class}) => FieldDefinition::Required(SimpleFieldDefinition::Document{typ: class, is_subdocument: true})
         }
     }
 }
-*/
 
 #[derive(Deserialize, PartialEq, Debug)]
 struct KeyDefinition;    
@@ -121,7 +148,7 @@ struct ClassDefinition {
     documentation: Option<ClassDocumentationDefinition>,
     key: Option<KeyDefinition>,
     #[serde(flatten)]
-    fields: BTreeMap<String, FieldDefinition>
+    fields: BTreeMap<String, StructuralFieldDefinition>
 }
 #[derive(Deserialize, PartialEq, Debug)]
 struct TaggedUnionDefinition;
