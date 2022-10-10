8ede94a83f60472b53ff42e2eaf5f5162840e60c
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-10-10 13:12:15 +0200

    update versions (#1499)
    * update versions
    
    * Update versions
    
    Co-authored-by: matko <matko@users.noreply.github.com>

--- src/config/terminus_config.pl
+++ src/config/terminus_config.pl
@@ -43,7 +43,7 @@
 :- use_module(library(yall)).
 
 /* [[[cog import cog; cog.out(f"terminusdb_version('{CURRENT_REPO_VERSION}').") ]]] */
-terminusdb_version('10.1.5').
+terminusdb_version('10.1.6').
 /* [[[end]]] */
 
 bootstrap_config_files :-
