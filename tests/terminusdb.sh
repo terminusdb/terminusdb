67dc9052829d19ff092d071ba1dd55590e3d9d0c
Robin de Rooij <rderooij685@gmail.com>
2022-11-09 20:01:21 +0100

    Don't spawn a new container for each CLI command

--- tests/terminusdb.sh
+++ tests/terminusdb.sh
@@ -9,35 +9,31 @@
 
 # Determine whether the $TERMINUSDB_DOCKER_IMAGE_TAG was passed and we should
 # use Docker or whether we have a valid executable path and we should use it.
-[[ "${TERMINUSDB_DOCKER_IMAGE_TAG:-x}" == "x" ]] && use_docker=1 || use_docker=0
+[[ "${TERMINUSDB_DOCKER_CONTAINER:-x}" == "x" ]] && use_docker=1 || use_running_docker=0
 [[ -x "${TERMINUSDB_EXEC_PATH:="../terminusdb"}" ]] && use_exec=0 || use_exec=1
 
 # If neither Docker nor executable, error.
 if [[ $use_docker -ne 0 && $use_exec -ne 0 ]]; then
-  echo "Error! Missing \$TERMINUSDB_DOCKER_IMAGE_TAG or executable ($TERMINUSDB_EXEC_PATH)."
+  echo "Error! Missing \$TERMINUSDB_DOCKER_CONTAINER or executable ($TERMINUSDB_EXEC_PATH)."
   exit -1
 fi
 
 if [[ $use_docker -eq 0 ]]; then
   # Use the Docker image.
-  if docker image inspect "$TERMINUSDB_DOCKER_IMAGE_TAG" &> /dev/null; then
+  if docker inspect "$TERMINUSDB_DOCKER_CONTAINER" &> /dev/null; then
     user="$(id -u):$(id -g)"
     set -e
     if [ -t 1 ]; then
       set -x
     fi
-    docker run $TERMINUSDB_EXTRA_DOCKER_PARAMS \
-      --rm \
+    docker exec "$TERMINUSDB_DOCKER_CONTAINER" \
       --user $user \
-      --network host \
-      --volume "$PWD:/app/terminusdb/tests" \
       --env TERMINUSDB_SERVER_DB_PATH="$TERMINUSDB_SERVER_DB_PATH" \
       --workdir /app/terminusdb/tests \
-      "$TERMINUSDB_DOCKER_IMAGE_TAG" \
       /app/terminusdb/terminusdb \
       "$@"
   else
-    echo "Error! \$TERMINUSDB_DOCKER_IMAGE_TAG does not have a valid image: $TERMINUSDB_DOCKER_IMAGE_TAG"
+    echo "Error! \$TERMINUSDB_DOCKER_CONTAINER does not have a valid name: $TERMINUSDB_DOCKER_CONTAINER"
     exit -1
   fi
 elif [[ $use_exec -eq 0 ]]; then
