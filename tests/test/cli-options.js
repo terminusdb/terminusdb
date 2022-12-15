96b48d65cd26a47744c706fa48f76e3bb58b8b93
Robin de Rooij <rrooij@users.noreply.github.com>
2022-12-15 13:27:54 +0100

    Greatly improve CI speed by parallelizing test in some degree (#1610)
    * Try to make tests more contained
    
    * tests: make all CLI tests self contained
    
    If they modify the global ENVs, then those affect other
    tests as well. I know construct new ENV storage paths for
    each CLI test.
    
    * Set timeout to bigger number and run one test separately
    
    There is no way in which we can test data-version while
    running other tests in parallel since the data-version will
    change before we check it.
    
    * document-auth: use the shared db approach
    
    * Uppdate mocha
    
    * Fix more tests to be self-contained
    
    * Fix DB removal in test
    
    * Fix test by not cleaning up bundles and using ENV
    
    This is in my opinion warranted since there are not
    that many files floating around from using the bundle
    tests.
    
    * Remove duplicated test
    
    This test is already present in cli-log.js
    
    * Refactor log test to prevent code duplication
    
    * ci: allow dynamic setting of parallel tests in mocha
    
    Set default to 4 because otherwise things blow up because
    the tests eat all the resources from TerminusDB itself.
    
    The nightly arm64 builds will not use it by default since it is
    slow.

--- tests/test/cli-options.js
+++ tests/test/cli-options.js
@@ -5,43 +5,50 @@ const { util } = require('../lib')
 
 describe('cli-options', function () {
   let dbSpec
+  let dbPath
+  let envs
+
+  async function execEnv (command) {
+    return exec(command, { env: envs })
+  }
+
   before(async function () {
     this.timeout(200000)
-    process.env.TERMINUSDB_SERVER_DB_PATH = './storage/' + util.randomString()
+    dbPath = './storage/' + util.randomString()
+    envs = { ...process.env, TERMINUSDB_SERVER_DB_PATH: dbPath }
     {
-      const r = await exec('./terminusdb.sh store init --force')
+      const r = await execEnv('./terminusdb.sh store init --force')
       expect(r.stdout).to.match(/^Successfully initialised database/)
     }
     dbSpec = `admin/${util.randomString()}`
     {
-      const r = await exec(`./terminusdb.sh db create ${dbSpec}`)
+      const r = await execEnv(`./terminusdb.sh db create ${dbSpec}`)
       expect(r.stdout).to.match(new RegExp(`^Database created: ${dbSpec}`))
     }
   })
 
   after(async function () {
-    const r = await exec(`./terminusdb.sh db delete ${dbSpec}`)
+    const r = await execEnv(`./terminusdb.sh db delete ${dbSpec}`)
     expect(r.stdout).to.match(new RegExp(`^Database deleted: ${dbSpec}`))
-    await fs.rm(process.env.TERMINUSDB_SERVER_DB_PATH, { recursive: true })
-    delete process.env.TERMINUSDB_SERVER_DB_PATH
+    await fs.rm(dbPath, { recursive: true })
   })
 
   describe('command line options report correct errors', function () {
     // This may depend on prolog version!
     it('nice error for bad switch', async function () {
-      const r = await exec('./terminusdb.sh db create admin/bar -g foo')
+      const r = await execEnv('./terminusdb.sh db create admin/bar -g foo')
       const errorPattern = /ERROR: The command line option "g" does not exist for the command "db create".*/
       expect(r.stderr).to.match(new RegExp(errorPattern))
     })
 
     it('nice error for bad type', async function () {
-      const r = await exec('./terminusdb.sh db create admin/bar --public=foo')
+      const r = await execEnv('./terminusdb.sh db create admin/bar --public=foo')
       const errorPattern = /ERROR: flag 'public': expected atom parsable as boolean, found 'foo'.*/
       expect(r.stdout).to.match(new RegExp(errorPattern))
     })
 
     it('nice error for bad shortflag option', async function () {
-      const r = await exec('./terminusdb.sh db list admin/bar -b=true')
+      const r = await execEnv('./terminusdb.sh db list admin/bar -b=true')
       const errorPattern = /ERROR: The command line does not accept -<shortflag>=<value> syntax in the command "db list".*/
       expect(r.stderr).to.match(new RegExp(errorPattern))
     })
