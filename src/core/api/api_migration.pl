42aee6d45d3c9d620810553a0cc443e947d7cbee
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-03-21 10:52:22 +0100

    Forgot to check in api_migration.pl

---
+++ src/core/api/api_migration.pl
@@ -0,0 +1,28 @@
+:- module(api_migration, [
+              api_hypothetical_migration/4,
+              api_migrate_resource/5,
+              api_transform_schema/3
+          ]).
+
+:- use_module(core(account)).
+:- use_module(core(document/migration)).
+:- use_module(core(util)).
+
+operations_string_to_list(Operations_String, Operations) :-
+    term_string(Term, Operations_String, [variable_names(VNames)]),
+    do_or_die(
+        VNames = [],
+        error(malformed_operations_string)),
+    xfy_list(',', Term, Operations).
+
+api_migrate_resource(System, Auth, Path, Commit_Info, Operations_String) :-
+    resolve_descriptor_auth(write, System, Auth, Path, instance, _Descriptor),
+    resolve_descriptor_auth(write, System, Auth, Path, schema, Descriptor),
+    operations_string_to_list(Operations_String, Operations),
+    perform_instance_migration(Descriptor, Commit_Info, Operations).
+
+api_hypothetical_migration(_System, _Auth, _Path, _New_Schema) :-
+    throw(unimplemented).
+
+api_transform_schema(_Schema, _Operations, _New_Schema) :-
+    throw(unimplemented).
