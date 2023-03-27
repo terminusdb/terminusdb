e341705c1a9399bb4938e98114e23bfb6945e75d
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-03-27 19:11:54 +0200

    First integration test with branch migration

--- tests/test/cli-schema-migration.js
+++ tests/test/cli-schema-migration.js
@@ -62,35 +62,56 @@ describe('cli-schema-migration', function () {
   it('target a migration of a branch', async function () {
     const instance = [
       {
+        '@id' : 'String/1',
         '@type': 'String',
         string: 'asdf',
       },
       {
+        '@id' : 'String/2',
         '@type': 'String',
         string: 'fdsa',
       },
       {
+        '@id' : 'Int/1',
         '@type': 'Int',
         int: 23,
       },
     ]
-    await execEnv(`./terminusdb.sh doc insert admin/${dbSpec} --data='${JSON.stringify(instance)}'`)
-    await execEnv(`./terminusdb.sh branch create admin/${dbSpec}/local/branch/foo`)
+    await execEnv(`./terminusdb.sh doc insert ${dbSpec} --data='${JSON.stringify(instance)}'`)
+    await execEnv(`./terminusdb.sh branch create ${dbSpec}/local/branch/foo`)
     const branchInstance = [
       {
+        '@id' : 'String/3',
         '@type': 'String',
         string: 'new',
       },
       {
+        '@id' : 'Int/2',
         '@type': 'Int',
         int: 42,
       },
     ]
-    await execEnv(`./terminusdb.sh doc insert admin/${dbSpec}/local/branch/foo --data='${JSON.stringify(branchInstance)}'`)
+    await execEnv(`./terminusdb.sh doc insert ${dbSpec}/local/branch/foo --data='${JSON.stringify(branchInstance)}'`)
 
-    await execEnv(`./terminusdb.sh migrate admin/${dbSpec} --operations='[drop_class_property("String", "string"), create_class_property("String", "string", {"@type" : "Optional", "@class" : "String"})]'`)
-    await execEnv(`./terminusdb.sh migrate admin/${dbSpec}/local/branch/foo --target admin/${dbSpec}`)
-    const r = execEnv(`./terminusdb.sh doc get admin/${dbSpec}/local/branch/foo`)
-    console.log(r.stdout)
+    const rMigration = await execEnv(`./terminusdb.sh migration ${dbSpec} --operations='delete_class_property("String", "string"), create_class_property("String", "string", {"@type" : "Optional", "@class" : "String"})'`)
+    expect(rMigration.stdout).to.match(/{"instance_operations":2, "schema_operations":2}/)
+
+    const rMain = await execEnv(`./terminusdb.sh doc get ${dbSpec} --as-list`)
+    const docsMain = JSON.parse(rMain.stdout)
+    expect(docsMain).to.deep.equal([
+      { '@id': 'Int/1', '@type': 'Int', int: 23 },
+      { '@id': 'String/1', '@type': 'String' },
+      { '@id': 'String/2', '@type': 'String' }
+    ])
+
+    const rBranchMigration = await execEnv(`./terminusdb.sh migration ${dbSpec}/local/branch/foo --target ${dbSpec}`)
+    expect(rBranchMigration.stdout).to.match(/{"instance_operations":3, "schema_operations":2}/)
+    const r = await execEnv(`./terminusdb.sh doc get ${dbSpec}/local/branch/foo --as-list`)
+    const docs = JSON.parse(r.stdout)
+    expect(docs).to.deep.equal([{"@id":"Int/1","@type":"Int","int":23},
+                                {"@id":"Int/2","@type":"Int","int":42},
+                                {"@id":"String/1","@type":"String"},
+                                {"@id":"String/2","@type":"String"},
+                                {"@id":"String/3","@type":"String"}])
   })
 })
