e383c7cbe4e0cf3e7d2c8329676cd5e27eb8f06d
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-04-07 02:15:09 +0200

    Fix linting and some genuine test errors

--- tests/test/history.js
+++ tests/test/history.js
@@ -21,17 +21,15 @@ describe('history', function () {
 
     it('gets a history from changes', async function () {
       const id = util.randomString()
-      const schema = { '@type': 'Class', '@id': id, 'a' : 'xsd:string'}
+      const schema = { '@type': 'Class', '@id': id, a: 'xsd:string' }
       await document.insert(agent, { schema })
-      const instance1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'a' }
-      const result1 = await document.insert(agent, { instance: instance1 })
-      const version1 = result1.headers['terminusdb-data-version'].split('branch:')[1]
+      const instance1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'a' }
+      await document.insert(agent, { instance: instance1 })
 
-      const instance2 = { '@type': id, '@id': `terminusdb:///data/${id}/1`, a : 'b' }
-      const result2 = await document.insert(agent, { instance: instance2 })
-      const version2 = result2.headers['terminusdb-data-version'].split('branch:')[1]
+      const instance2 = { '@type': id, '@id': `terminusdb:///data/${id}/1`, a: 'b' }
+      await document.insert(agent, { instance: instance2 })
 
-      const instance1dot0 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'c' }
+      const instance1dot0 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'c' }
       const result3 = await document.replace(agent, { instance: instance1dot0 })
       const version3 = result3.headers['terminusdb-data-version'].split('branch:')[1]
 
@@ -45,17 +43,16 @@ describe('history', function () {
 
     it('finds created and updated date', async function () {
       const id = util.randomString()
-      const schema = { '@type': 'Class', '@id': id, 'a' : 'xsd:string'}
+      const schema = { '@type': 'Class', '@id': id, a: 'xsd:string' }
       await document.insert(agent, { schema })
-      const instance1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'a' }
+      const instance1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'a' }
       const result1 = await document.insert(agent, { instance: instance1 })
       const version1 = result1.headers['terminusdb-data-version'].split('branch:')[1]
 
-      const instance2 = { '@type': id, '@id': `terminusdb:///data/${id}/1`, a : 'b' }
-      const result2 = await document.insert(agent, { instance: instance2 })
-      const version2 = result2.headers['terminusdb-data-version'].split('branch:')[1]
+      const instance2 = { '@type': id, '@id': `terminusdb:///data/${id}/1`, a: 'b' }
+      await document.insert(agent, { instance: instance2 })
 
-      const instance1dot0 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'c' }
+      const instance1dot0 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'c' }
       const result3 = await document.replace(agent, { instance: instance1dot0 })
       const version3 = result3.headers['terminusdb-data-version'].split('branch:')[1]
 
@@ -66,42 +63,46 @@ describe('history', function () {
       expect(history.updated.identifier).to.equal(version3)
     })
 
-
     it('pages history', async function () {
       const id = util.randomString()
-      const schema = { '@type': 'Class', '@id': id, 'a' : 'xsd:string'}
+      const schema = { '@type': 'Class', '@id': id, a: 'xsd:string' }
       await document.insert(agent, { schema })
-      const instance1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'a' }
+      const instance1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'a' }
       const result1 = await document.insert(agent, { instance: instance1 })
       const version1 = result1.headers['terminusdb-data-version'].split('branch:')[1]
 
-      const instance1dot0 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'b' }
+      const instance1dot0 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'b' }
       const result2 = await document.replace(agent, { instance: instance1dot0 })
       const version2 = result2.headers['terminusdb-data-version'].split('branch:')[1]
 
-      const instance1dot1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'c' }
+      const instance1dot1 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'c' }
       const result3 = await document.replace(agent, { instance: instance1dot1 })
       const version3 = result3.headers['terminusdb-data-version'].split('branch:')[1]
 
-      const instance1dot2 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a : 'd' }
-      const result4 = await document.replace(agent, { instance: instance1dot0 })
+      const instance1dot2 = { '@type': id, '@id': `terminusdb:///data/${id}/0`, a: 'd' }
+      const result4 = await document.replace(agent, { instance: instance1dot2 })
       const version4 = result4.headers['terminusdb-data-version'].split('branch:')[1]
 
       const historyRequest1 = await agent.get(`/api/history/admin/${dbName}?id=${id}%2F0&start=0`)
       const history0toInf = historyRequest1.body
       expect(history0toInf).to.have.lengthOf(4)
+      expect(history0toInf[0].identifier).to.equal(version4)
 
       const historyRequest2 = await agent.get(`/api/history/admin/${dbName}?id=${id}%2F0&start=0&count=1`)
       const history0to1 = historyRequest2.body
       expect(history0to1).to.have.lengthOf(1)
 
-
       const historyRequest3 = await agent.get(`/api/history/admin/${dbName}?id=${id}%2F0&start=1&count=2`)
-      const history1to2 = historyRequest3.body
-      expect(history1to2).to.have.lengthOf(2)
-      expect(history1to2[0].identifier).to.equal(version3)
-      expect(history1to2[1].identifier).to.equal(version2)
-    })
+      const history1to3 = historyRequest3.body
+      expect(history1to3).to.have.lengthOf(2)
+      expect(history1to3[0].identifier).to.equal(version3)
+      expect(history1to3[1].identifier).to.equal(version2)
 
+      const historyRequest4 = await agent.get(`/api/history/admin/${dbName}?id=${id}%2F0&start=2&count=2`)
+      const history3to4 = historyRequest4.body
+      expect(history3to4).to.have.lengthOf(2)
+      expect(history3to4[0].identifier).to.equal(version2)
+      expect(history3to4[1].identifier).to.equal(version1)
+    })
   })
 })
