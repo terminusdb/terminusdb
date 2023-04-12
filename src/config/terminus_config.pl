aecce56a8b6c655bba5ab0f7816ece56ed6440aa
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-04-12 16:39:50 +0200

    Raise version to v11.0.6 (#1790)
    * Raise version to v11.0.6
    
    * Update versions
    
    * trigger workflow
    
    ---------
    
    Co-authored-by: matko <matko@users.noreply.github.com>

--- src/config/terminus_config.pl
+++ src/config/terminus_config.pl
@@ -48,7 +48,7 @@
 
 
 /* [[[cog import cog; cog.out(f"terminusdb_version('{CURRENT_REPO_VERSION}').") ]]] */
-terminusdb_version('11.0.5').
+terminusdb_version('11.0.6').
 /* [[[end]]] */
 
 bootstrap_config_files :-
