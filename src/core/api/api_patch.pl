fe055f5e0238d84a76de0f79d92aee71ddd2ede3
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-04-25 14:30:09 +0200

    Removing traces

--- src/core/api/api_patch.pl
+++ src/core/api/api_patch.pl
@@ -204,7 +204,6 @@ api_diff_all_documents(System_DB, Auth, Path, Before_Version, After_Version, Dif
     resolve_descriptor_auth(read, System_DB, Auth, Path, instance, Branch_Descriptor),
     coerce_to_commit(Branch_Descriptor, Before_Version, Before_Commit_Id),
     coerce_to_commit(Branch_Descriptor, After_Version, After_Commit_Id),
-    trace(document_diffs_from_commits),
     findall(Diff,
             document_diffs_from_commits(Branch_Descriptor,
                                         Before_Commit_Id,
@@ -222,8 +221,6 @@ api_apply_squash_commit(System_DB, Auth, Path, Commit_Info, Before_Version, Afte
     merge_options(Options, options{keep:json{'@id':true, '@type':true},
                                    count:inf,
                                    start:0}, Merged_Options),
-    trace(document_diffs_from_commits),
-    trace(apply_diff),
     with_transaction(
         Context,
         (   findall(Witness,
