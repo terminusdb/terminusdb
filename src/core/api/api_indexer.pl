03305cb7d731c5d69c65c7be23061d2580bfb649
Robin de Rooij <rderooij685@gmail.com>
2023-06-22 10:52:19 +0200

    Fix missing import for superuser check

--- src/core/api/api_indexer.pl
+++ src/core/api/api_indexer.pl
@@ -11,6 +11,7 @@
 :- use_module(core(query)).
 :- use_module(core(transaction)).
 :- use_module(core(util)).
+:- use_module(core(account)).
 :- use_module(library(http/json)).
 :- use_module(library(http/http_client)).
 :- use_module(core(api/api_graphql)).
