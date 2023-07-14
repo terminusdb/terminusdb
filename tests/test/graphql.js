a704a96885683cea284feae523cb7c7e8d3ad578
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-07-14 16:38:28 +0200

    Fix test

--- tests/test/graphql.js
+++ tests/test/graphql.js
@@ -735,10 +735,6 @@ query EverythingQuery {
     })
 
     it('graphql no subsumption', async function () {
-      const members = [{ name: 'Joe', number: 3 },
-        { name: 'Jim', number: 5 },
-        { '@type': 'Parent', name: 'Dad' }]
-      await document.insert(agent, { instance: members })
       const PARENT_QUERY = gql`
  query ParentQuery {
     Parent(include_children: false, orderBy: {name : ASC}){
