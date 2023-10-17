aecb54ad8abfefc7d3f90b2d1389c4ce4a70accf
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-10-17 17:09:05 +0200

    Raise version to v11.1.5 (#1983)
    * Raise version to v11.1.5
    
    * Update versions
    
    * trigger ci
    
    ---------
    
    Co-authored-by: matko <matko@users.noreply.github.com>

--- src/config/terminus_config.pl
+++ src/config/terminus_config.pl
@@ -49,7 +49,7 @@
 
 
 /* [[[cog import cog; cog.out(f"terminusdb_version('{CURRENT_REPO_VERSION}').") ]]] */
-terminusdb_version('11.1.4').
+terminusdb_version('11.1.5').
 /* [[[end]]] */
 
 bootstrap_config_files :-
