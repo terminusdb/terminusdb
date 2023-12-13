faf810c801d380c564779fd73995ecc4b0bb2c9a
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-12-13 07:55:28 +0100

    transcode directly from json to term and check errors

--- src/rust/terminusdb-community/src/json.rs
+++ src/rust/terminusdb-community/src/json.rs
@@ -1,18 +1,72 @@
-use std::io::{BufReader, Read};
-use swipl::{prelude::*, term::ser::to_term_with_config};
+use std::{cell::Cell, io::Read};
+
+use swipl::{prelude::*, term::de};
+
+enum TranscodeOutput {
+    Element,
+    Eof,
+}
+
+fn translate_transcode_error<C: QueryableContextType>(
+    context: &Context<C>,
+    result: Result<(), de::Error>,
+    non_whitespace_detected: bool,
+) -> PrologResult<TranscodeOutput> {
+    match result {
+        Ok(_) => Ok(TranscodeOutput::Element),
+        // annoying to have to match a string like that but we
+        // unfortunately do not retain the original error due to how
+        // transcoding works
+        Err(de::Error::Message(m)) if !non_whitespace_detected && m.starts_with("EOF") => {
+            Ok(TranscodeOutput::Eof)
+        }
+        Err(de::Error::Message(m)) => {
+            context.raise_exception(&term! {context: syntax_error(invalid_json(#m))}?)
+        }
+        Err(de::Error::UnificationFailed) => Err(PrologError::Failure),
+        Err(e) => context.try_or_die_generic(Err(e)),
+    }
+}
+
+struct NonWhitespaceDetector<'a, R> {
+    canary: &'a Cell<bool>,
+    inner: R,
+}
+
+impl<'a, R: Read> Read for NonWhitespaceDetector<'a, R> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let result = self.inner.read(buf);
+
+        if !self.canary.get() {
+            if let Ok(size) = result.as_ref() {
+                // let's see if any of the read data is non-whitespace
+                if buf[..*size].iter().any(|c| !c.is_ascii_whitespace()) {
+                    self.canary.set(true);
+                }
+            }
+        }
+
+        result
+    }
+}
 
 predicates! {
     #[module("$util")]
     semidet fn json_read_dict_fast(context, stream_term, doc_term) {
         let stream: ReadablePrologStream = stream_term.get_ex()?;
-        let mut json_deserializer = serde_json::Deserializer::from_reader(stream.decoding_reader()).into_iter::<serde_json::Value>();
-        let json = match json_deserializer.next() {
-            Some(result) => context.try_or_die_generic(result)?,
-            None => return doc_term.unify(atom!("eof"))
+        let whitespace_canary = Cell::new(false);
+        let reader = NonWhitespaceDetector {
+            inner: stream.decoding_reader(),
+            canary: &whitespace_canary,
         };
-        let config = SerializerConfiguration::new().default_tag(atom!("json"));
+        let mut json_deserializer = serde_json::Deserializer::from_reader(reader);
+        let config = SerializerConfiguration::new().default_tag(atom!("json")).unit_atom(atom!("null"));
+        let serializer = swipl::term::ser::Serializer::new_with_config(context, doc_term.clone(), config);
+        if matches!(translate_transcode_error(context, serde_transcode::transcode(&mut json_deserializer, serializer), whitespace_canary.get())?, TranscodeOutput::Eof) {
+            doc_term.unify(atom!("eof"))?;
+        }
 
-        context.try_or_die_generic(to_term_with_config(context, doc_term, &json, config))
+        Ok(())
     }
 }
 
