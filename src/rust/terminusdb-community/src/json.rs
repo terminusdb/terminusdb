2e5ba43027716b59192c3efb0ea63a39f06d4e94
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-12-13 06:20:22 +0100

    properly decode latin1

--- src/rust/terminusdb-community/src/json.rs
+++ src/rust/terminusdb-community/src/json.rs
@@ -1,10 +1,11 @@
+use std::io::{BufReader, Read};
 use swipl::{prelude::*, term::ser::to_term_with_config};
 
 predicates! {
     #[module("$util")]
     semidet fn json_read_dict_fast(context, stream_term, doc_term) {
         let stream: ReadablePrologStream = stream_term.get_ex()?;
-        let mut json_deserializer = serde_json::Deserializer::from_reader(stream).into_iter::<serde_json::Value>();
+        let mut json_deserializer = serde_json::Deserializer::from_reader(stream.decoding_reader()).into_iter::<serde_json::Value>();
         let json = match json_deserializer.next() {
             Some(result) => context.try_or_die_generic(result)?,
             None => return doc_term.unify(atom!("eof"))
