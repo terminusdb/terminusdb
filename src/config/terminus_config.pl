f99ba30c1be741daf86cee70ed6649195bb78a69
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-01-11 13:48:23 +0100

    Bump version (#2083)
    * Bump version
    
    * empty commit
    
    * Update versions
    
    * Empty commit
    
    ---------
    
    Co-authored-by: GavinMendelGleason <GavinMendelGleason@users.noreply.github.com>

--- src/config/terminus_config.pl
+++ src/config/terminus_config.pl
@@ -49,7 +49,7 @@
 
 
 /* [[[cog import cog; cog.out(f"terminusdb_version('{CURRENT_REPO_VERSION}').") ]]] */
-terminusdb_version('11.1.11').
+terminusdb_version('11.1.12').
 /* [[[end]]] */
 
 bootstrap_config_files :-
