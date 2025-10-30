9da1f2ab6c6d4c204388d12a74bc7a595a3e65ca
Philippe Höij <philippe@hoij.net>
2025-10-30 21:50:43 +0100

    Add info about tus pack

--- distribution/init_snap.sh
+++ distribution/init_snap.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 export SWI_HOME_DIR="$SNAP/usr/lib/swi-prolog"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+LD_LIBRARY_PATH:}${SWI_HOME_DIR}/lib/x86_64-linux/"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}${SWI_HOME_DIR}/lib/x86_64-linux/"
+
+# DEBUG: Check if TUS exists
+if [ -d "$SNAP/tus" ]; then
+    echo "TUS pack found at $SNAP/tus"
+    ls -la "$SNAP/tus/"
+else
+    echo "ERROR: TUS pack not found at $SNAP/tus"
+fi
+
 $SNAP/terminusdb "$@"
