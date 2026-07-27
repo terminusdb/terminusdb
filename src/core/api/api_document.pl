5ed4d3f78d1e5cef29d37064affb28b5b42bb5ed
Philippe Höij <philippe.hoij@dfrnt.com>
2026-07-27 08:31:26 +0200

    Proper loading of json2markdown module

--- src/core/api/api_document.pl
+++ src/core/api/api_document.pl
@@ -78,7 +78,6 @@
 :- use_module(core(util/json_preserve), [close_list_tails/1]).
 :- use_module(core(account)).
 :- use_module(core(plugins)).
-:- use_module(core(json2markdown)).
 :- use_module(config(terminus_config)).
 
 :- use_module(library(terminus_store)).
@@ -1540,7 +1539,11 @@ document_stream_start(markdown, _Config, StreamState) :-
 
 document_stream_write(markdown, _Config, state, Document) :-
     strip_metadata_fields(Document, Cleaned),
-    json2markdown:json_to_markdown(Cleaned, Markdown),
+    (   current_predicate(json2markdown:json_to_markdown/2),
+        json2markdown:json_to_markdown(Cleaned, Markdown)
+    ->  true
+    ;   Markdown = ''
+    ),
     format("~w~n", [Markdown]).
 
 document_stream_end(markdown, _Config).
